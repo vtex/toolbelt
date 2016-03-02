6a8a38c70a30ebf112913f1d77e40470d34bc098
Thor Amorim <thoramorim@gmail.com>
2016-03-02 19:19:31 -0300

    Update to beta gallery endpoints

--- src/lib/watch.js
+++ src/lib/watch.js
@@ -24,8 +24,8 @@ class Watcher {
     this.credentials = credentials;
     this.isServerSet = isServerSet;
 
-    this.appsEndpoint = 'http://apps.vtex.com';
-    this.workspacesEndpoint = 'http://workspaces.vtex.com';
+    this.appsEndpoint = 'http://apps.beta.vtex.com';
+    this.workspacesEndpoint = 'http://workspaces.beta.vtex.com';
     this.acceptHeader = 'application/vnd.vtex.workspaces.v0+json';
     this.sandbox = this.credentials.email;
     this.workspace = 'sb_' + this.credentials.email;
@@ -399,12 +399,15 @@ class Watcher {
   activateSandbox = () => {
     let deferred = Q.defer();
     let options = {
-      url: (this.workspacesEndpoint + '/' + this.credentials.account + '/workspaces/' + this.workspace + '/') + ('sandboxes/' + this.vendor + '/' + this.credentials.email + '/apps/' + this.app),
+      url: this.appsEndpoint + '/' + this.vendor + '/sandboxes/' + this.sandbox + '/' + this.app + '/ttl/' + this.credentials.account + '/' + this.workspace,
       method: 'PUT',
       headers: {
         Authorization: 'token ' + this.credentials.token,
         Accept: this.acceptHeader,
         'Content-Type': 'application/json'
+      },
+      body: {
+        ttl: 30000
       }
     };
 
@@ -422,7 +425,7 @@ class Watcher {
 
   deactivateSandbox = () => {
     let options = {
-      url: (this.workspacesEndpoint + '/' + this.credentials.account + '/workspaces/' + this.workspace + '/') + ('sandboxes/' + this.vendor + '/' + this.credentials.email + '/apps/' + this.app),
+      url: this.appsEndpoint + '/' + this.vendor + '/sandboxes/' + this.sandbox + '/' + this.app + '/ttl/' + this.credentials.account + '/' + this.workspace,
       method: 'DELETE',
       headers: {
         Authorization: 'token ' + this.credentials.token,
