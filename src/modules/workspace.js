779909635bbcba451b791f9e225ad8107d260a10
Guilherme Rodrigues <gadr90@gmail.com>
2016-06-02 15:49:32 +0200

    List workspaces

--- src/modules/workspace.js
+++ src/modules/workspace.js
@@ -1,7 +1,25 @@
+import {WorkspacesClient} from '@vtex/workspaces'
+import {map, prop} from 'ramda'
 import log from '../logger'
+import {getToken, getAccount} from '../conf'
+import pkg from '../../package.json'
+
+const client = new WorkspacesClient({
+  authToken: getToken(),
+  userAgent: `toolbelt-v-${pkg.version}`,
+})
 
 export default {
   workspace: {
+    list: {
+      description: 'List workspaces on this account',
+      handler: () => {
+        log.debug('Listing workspaces')
+        client.list(getAccount()).then(res => {
+          console.log(map(prop('name'), res.body).join('\n'))
+        })
+      },
+    },
     new: {
       requiredArgs: 'name',
       description: 'Create a new workspace with this name',
