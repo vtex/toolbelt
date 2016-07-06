d12d80f11cd3b4daa0c992c861bdf678ad6190c8
Guilherme Rodrigues <gadr90@gmail.com>
2016-07-06 16:20:28 +0200

    Do not ask to login before login, derp

--- src/cli.js
+++ src/cli.js
@@ -26,7 +26,9 @@ const checkCommandExists = found => {
 }
 
 const checkLogin = found => {
-  if (found.command !== tree && !getToken()) {
+  const whitelist = [tree, tree.login]
+  if (!getToken() && whitelist.indexOf(found.command) === -1) {
+    log.debug('Requesting login before command:', process.argv.slice(2).join(' '))
     return run({command: tree.login})
   }
 }
