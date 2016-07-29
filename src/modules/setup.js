b1c759b6fc8ea57cd90106039135e39746958c38
Guilherme Rodrigues <gadr90@gmail.com>
2016-07-29 17:02:39 +0200

    Add eslint node modules

--- src/modules/setup.js
+++ src/modules/setup.js
@@ -1,24 +1,19 @@
 import fs from 'fs'
-import path from 'path'
+import {basename, dirname, join} from 'path'
 import log from '../logger'
-import {flip, curry} from 'ramda'
 import {Promise, promisify, all} from 'bluebird'
 
 const mkdir = promisify(fs.mkdir)
 const symlink = promisify(fs.symlink)
-const flippedJoin = curry(flip(path.join))
-const joinEslint = flippedJoin('eslint')
-const joinEslintRc = flippedJoin('.eslintrc')
-const joinBin = flippedJoin('node_modules/.bin')
-
-function linkEslint (origin, dest) {
-  const destBin = joinBin(dest)
-  const originBin = joinBin(origin)
-  return all([
-    symlink(joinEslintRc(origin), joinEslintRc(dest)),
-    symlink(joinEslint(originBin), joinEslint(destBin)),
-  ])
-}
+const eslintAssets = [
+  '.eslintrc',
+  'node_modules/eslint',
+  'node_modules/.bin/eslint',
+  'node_modules/babel-eslint',
+  'node_modules/.bin/babel-eslint',
+]
+const linkEslint = (origin, dest) =>
+  all(eslintAssets.map(a => symlink(join(origin, a), join(dest, a))))
 
 export default {
   setup: {
@@ -27,10 +22,9 @@ export default {
       handler: () => {
         log.info('Linking eslint setup...')
         const root = process.cwd()
-        const rootBin = joinBin(root)
-        const pkg = path.join(__dirname, '../..')
-        return mkdir(path.join(root, 'node_modules'))
-        .then(() => mkdir(rootBin))
+        const pkg = join(__dirname, '../..')
+        return mkdir(join(root, 'node_modules'))
+        .then(() => mkdir(join(root, 'node_modules/.bin')))
         .catch(err => {
           return err.code && err.code === 'EEXIST'
             ? Promise.resolve()
@@ -40,7 +34,7 @@ export default {
         .then(() => log.info('Succesfully linked eslint setup!'))
         .catch(err => {
           if (err.code && err.code === 'EEXIST') {
-            log.error(`${path.basename(err.dest)} already exists on ${path.dirname(err.dest)}`)
+            log.error(`${basename(err.dest)} already exists on ${dirname(err.dest)}`)
             log.error('Remove your current eslint setup and run this command again')
             return process.exit(1)
           }
