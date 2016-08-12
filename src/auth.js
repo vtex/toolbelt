d3877b97e6a19c837ca020bc3275a526700a5f63
Guilherme Rodrigues <gadr90@gmail.com>
2016-08-12 20:06:53 +0200

    Move vtexid calls to @vtex/api

--- src/auth.js
+++ src/auth.js
@@ -1,34 +1,11 @@
-import http from './http'
+import userAgent from './user-agent'
+import {VTEXIDClient} from '@vtex/api'
 import {Promise} from 'bluebird'
-import {prop} from 'ramda'
 import log from './logger'
 
-export function getTemporaryToken () {
-  return http('https://vtexid.vtex.com.br/api/vtexid/pub/authentication/start')
-    .json()
-    .then(prop('authenticationToken'))
-}
-
-export function sendCodeToEmail (token, email) {
-  log.debug('Sending code to email', {token, email})
-  return http('https://vtexid.vtex.com.br/api/vtexid/pub/authentication/accesskey/send')
-    .query({authenticationToken: token, email})
-    .json()
-}
-
-export function getEmailCodeAuthenticationToken (token, email, code) {
-  log.debug('Getting auth token with email code', {token, email, code})
-  return http('https://vtexid.vtex.com.br/api/vtexid/pub/authentication/accesskey/validate')
-    .query({login: email, accesskey: code, authenticationToken: token})
-    .json()
-}
-
-export function getPasswordAuthenticationToken (token, email, password) {
-  log.debug('Getting auth token with password', {token, email, password})
-  return http('https://vtexid.vtex.com.br/api/vtexid/pub/authentication/classic/validate')
-    .query({authenticationToken: encodeURIComponent(token), login: encodeURIComponent(email), password: encodeURIComponent(password)})
-    .json()
-}
+const vtexid = new VTEXIDClient({
+  userAgent: userAgent,
+})
 
 export function isVtexUser (email) {
   return email.indexOf('@vtex.com') >= 0
@@ -43,23 +20,26 @@ export function handleAuthResult (result) {
 
 export function vtexUserAuth (email, prompt) {
   let token
-  return getTemporaryToken()
+  return vtexid.getTemporaryToken()
   .then(t => { token = t })
   .then(() => [token, email])
-  .spread(sendCodeToEmail)
+  .tap(() => log.debug('Sending code to email', {token, email}))
+  .spread(vtexid.sendCodeToEmail.bind(vtexid))
   .then(prompt)
   .then(({code}) => [token, email, code])
-  .spread(getEmailCodeAuthenticationToken)
+  .tap(() => log.debug('Getting auth token with email code', {token, email}))
+  .spread(vtexid.getEmailCodeAuthenticationToken.bind(vtexid))
   .then(handleAuthResult)
 }
 
 export function userAuth (email, prompt) {
   let token
-  return getTemporaryToken()
+  return vtexid.getTemporaryToken()
   .then(t => { token = t })
   .then(prompt)
   .then(({password}) => [token, email, password])
-  .spread(getPasswordAuthenticationToken)
+  .tap(() => log.debug('Getting auth token with password', {token, email}))
+  .spread(vtexid.getPasswordAuthenticationToken.bind(vtexid))
   .then(handleAuthResult)
 }
 
