7093ef2731d8fca2ff1f05af457bfb8b2318e27e
Thor Amorim <thoramorim@gmail.com>
2016-09-30 19:23:04 -0300

    Make workspace argument optional

--- src/modules/workspace.js
+++ src/modules/workspace.js
@@ -161,13 +161,14 @@ export default {
       },
     },
     reset: {
-      requiredArgs: 'name',
+      optionalArgs: 'name',
       description: 'Delete and create a workspace',
       handler: function (name) {
         log.debug('Resetting workspace', name)
-        return this.workspace.delete.handler(name, {yes: true, force: true})
+        const workspace = typeof name !== 'string' ? getWorkspace() : name
+        return this.workspace.delete.handler(workspace, {yes: true, force: true})
         .delay(3000)
-        .then(() => this.workspace.create.handler(name))
+        .then(() => this.workspace.create.handler(workspace))
       },
     },
   },
