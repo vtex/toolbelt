0657bd3985c1d4dee7cdd4d15f3a48dd55c2600f
Rodrigo Dumont <rlsdumont@gmail.com>
2016-10-19 11:38:25 -0200

    courier: remove redundant reconnection

--- src/courier.js
+++ src/courier.js
@@ -16,18 +16,6 @@ const levelFormat = {
   error: log.error,
 }
 
-const maxRetries = 30
-let retryCount = 0
-const getInterval = () => 2000 * Math.pow(1.15, retryCount)
-
-let retry = (account, workspace, authToken) => {
-  let interval = getInterval()
-  if (++retryCount <= maxRetries) {
-    setTimeout(() => listen(account, workspace, authToken), interval)
-    log.debug(`courier: will retry in ${Math.round(interval / 100) / 10} seconds`)
-  }
-}
-
 const clearAbove = () => {
   clearLine(process.stdout, 0)
   cursorTo(process.stdout, 0)
@@ -101,16 +89,9 @@ const listen = (account, workspace, authToken) => {
 
     clearAbove()
     logToConsole(level, origin, message)
-    retryCount = 0
-  })
-  es.addEventListener('close', (msg) => {
-    log.debug(`courier: connection closed. Error: ${msg.data}`)
-    es.close()
-    retry(account, workspace, authToken)
   })
   es.onerror = (err) => {
-    log.debug(`courier: failed to connect with status ${err.status}`)
-    retry(account, workspace, authToken)
+    log.error(`Connection to courier server has failed with status ${err.status}`)
   }
 }
 
