b9456499c6fe12d7da95429b5dcaaeaa4e88e7ee
Guilherme Rodrigues <gadr90@gmail.com>
2016-10-20 18:44:08 -0200

    Fix URL hint

--- src/workspace.test.js
+++ src/workspace.test.js
@@ -4,7 +4,7 @@ import {getWorkspaceURL} from './workspace'
 test('makes a development workspace from the login email', t => {
   const account = 'dreamstore'
   const workspace = 'test'
-  const expected = `http://${account}.alpha.myvtex.com/?vtex_workspace=${workspace}`
+  const expected = `http://${account}.myvtex.com/?vtex_workspace=${workspace}`
   const actual = getWorkspaceURL(account, workspace)
   t.is(actual, expected)
 })
