cde8159b13402bb05d75b1ffc560d181756cb095
Guilherme Rodrigues <gadr90@gmail.com>
2017-01-05 15:20:47 -0200

    Use new clients

--- src/modules/apps/utils.js
+++ src/modules/apps/utils.js
@@ -3,25 +3,21 @@ import {createReadStream} from 'fs'
 import {join} from 'path'
 import {manifest} from '../../manifest'
 import userAgent from '../../user-agent'
-import {VBaseClient, AppsClient, AppEngineClient, RegistryClient} from '@vtex/api'
+import {VBaseClient, AppEngineClient, AppRegistryClient} from '@vtex/api'
 import {getToken, getAccount, getWorkspace} from '../../conf'
 import endpoint from '../../endpoint'
 import timeout from '../../timeout'
 
-const appsBaseUrl = 'http://apps-engine-env-beta.us-east-1.elasticbeanstalk.com'
-
 export const id = `${manifest.vendor}.${manifest.name}@${manifest.version}`
-
-export const appsClient = () => new AppsClient(appsBaseUrl, {authToken: getToken(), userAgent, timeout})
 
 export const appEngineClient = () => new AppEngineClient(endpoint('apps'), {authToken: getToken(), userAgent, timeout})
 
-export const registryClient = () => new RegistryClient(endpoint('registry'), {authToken: getToken(), userAgent, timeout})
+export const registryClient = () => new AppRegistryClient(endpoint('registry'), {authToken: getToken(), userAgent, timeout})
 
 export const vbaseClient = () => new VBaseClient(endpoint('api'), {authToken: getToken(), userAgent, timeout})
 
 export const installApp = (id) =>
-  appsClient().installApp(
+  appEngineClient().installApp(
     getAccount(),
     getWorkspace(),
     {id}
