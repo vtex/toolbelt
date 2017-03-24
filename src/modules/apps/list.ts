f064243ea523787d735d94c92a8fa47c486208eb
Thor Amorim <thoramorim@gmail.com>
2017-03-24 18:00:00 -0300

    Restyle apps list output for linked apps

--- src/modules/apps/list.ts
+++ src/modules/apps/list.ts
@@ -1,34 +1,47 @@
-import {prop} from 'ramda'
 import * as chalk from 'chalk'
 import * as Table from 'cli-table'
+import {curry, compose, map, prop, split, head, length, gt, flip} from 'ramda'
 
 import log from '../../logger'
 import {apps} from '../../clients'
 import {parseLocator} from '../../locator'
 
-const {listApps, listLinks} = apps
+const {listApps} = apps
 
-const renderTable = (apps, title: string, emptyMessage: string): void => {
-  console.log(chalk.green(title))
-  if (apps.length === 0) {
-    return console.log(`${emptyMessage}\n`)
-  }
-  const table = new Table({head: ['Vendor', 'Name', 'Version']})
-  apps.forEach(r => table.push([r.vendor, r.name, r.version]))
-  console.log(`${table.toString()}\n`)
-}
+const flippedGt = flip(gt)
+
+const parseLocatorFromList =
+  map(compose<any, string, Manifest>(parseLocator, prop('app')))
+
+const cleanVersion =
+  compose<string, string[], string>(head, split('+build'))
+
+const isLinked =
+  compose<string, string[], number, boolean>(flippedGt(1), length, split('+build'))
+
+const renderTable = curry<string, string, any, void>(
+  (title: string, emptyMessage: string, apps): void => {
+    console.log(chalk.green(title))
+    if (apps.length === 0) {
+      return console.log(`${emptyMessage}\n`)
+    }
+    const table = new Table({head: ['Vendor', 'Name', 'Version', 'Linked']})
+    apps.forEach(({vendor, name, version}) => {
+      const linked = isLinked(version) ? chalk.green('yes') : 'no'
+      const cleanedVersion = cleanVersion(version)
+      table.push([vendor, name, cleanedVersion, linked])
+    })
+    console.log(`${table.toString()}\n`)
+  },
+)
 
 export default {
   description: 'List your installed VTEX apps',
   handler: () => {
     log.debug('Starting to list apps')
-    return Promise.all([
-      listApps().then(prop('data')).then(a => a.map(({app}) => parseLocator(app))),
-      listLinks().then(linkedApps => linkedApps.map(parseLocator)),
-    ])
-    .spread((installedApps: Manifest[], linkedApps: Manifest[]) => {
-      renderTable(installedApps, 'Installed Apps', 'You have no installed apps')
-      renderTable(linkedApps, 'Linked Apps', 'You have no linked apps')
-    })
+    return listApps()
+      .then(prop('data'))
+      .then(parseLocatorFromList)
+      .then(renderTable('Installed Apps', 'You have no installed apps'))
   },
 }
