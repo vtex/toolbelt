a5ea3212e4020547ee406214aed3eaf5661719c9
Thor Amorim <thoramorim@gmail.com>
2017-03-24 18:01:28 -0300

    Rewrite promote command

--- src/modules/workspace/promote.ts
+++ src/modules/workspace/promote.ts
@@ -1,26 +1,60 @@
 import * as chalk from 'chalk'
 import * as inquirer from 'inquirer'
+import * as Bluebird from 'bluebird'
+import {compose, flip, gt, length} from 'ramda'
 
 import useCmd from './use'
 import log from '../../logger'
-import {getAccount} from '../../conf'
-import {workspaces} from '../../clients'
+import {apps, workspaces} from '../../clients'
+import {getAccount, getWorkspace} from '../../conf'
+
+const {listLinks} = apps
+const {promote} = workspaces
+const [account, currentWorkspace] = [getAccount(), getWorkspace()]
+
+const flippedGt = flip(gt)
+
+const hasLinks =
+  compose<any[], number, boolean>(flippedGt(0), length)
+
+const isPromotable = (): Bluebird<never | void> =>
+  listLinks()
+    .then(hasLinks)
+    .then(result => {
+      if (!result) {
+        return
+      }
+      const err = new Error()
+      err.name = 'InterruptionError'
+      log.error('You have links on your workspace, please unlink them before promoting')
+      throw err
+    })
+
+const promptConfirm = (workspace: string): Bluebird<never | void> =>
+  Promise.resolve(
+    inquirer.prompt({
+      type: 'confirm',
+      name: 'confirm',
+      message: `Are you sure you want to promote workspace ${chalk.green(workspace)} to master?`,
+    }),
+  )
+  .then(({confirm}) => {
+    if (confirm) {
+      return
+    }
+    const err = new Error()
+    err.name = `InterruptionError`
+    throw err
+  })
 
 export default {
-  requiredArgs: 'name',
   description: 'Promote this workspace to master',
-  handler: (name: string) => {
-    log.debug('Promoting workspace', name)
-    return Promise.resolve(
-      inquirer.prompt({
-        type: 'confirm',
-        name: 'confirm',
-        message: `Are you sure you want to promote workspace ${chalk.green(name)} to master?`,
-      }),
-    )
-    .then(({confirm}) => confirm || Promise.reject('User cancelled'))
-    .then(() => workspaces.promote(getAccount(), name))
-    .tap(() => log.info(`Workspace ${chalk.green(name)} promoted successfully`))
-    .then(() => useCmd.handler('master'))
+  handler: () => {
+    log.debug('Promoting workspace', currentWorkspace)
+    return isPromotable()
+      .then(() => promptConfirm(currentWorkspace))
+      .then(() => promote(account, currentWorkspace))
+      .tap(() => log.info(`Workspace ${chalk.green(currentWorkspace)} promoted successfully`))
+      .then(() => useCmd.handler('master'))
   },
 }
