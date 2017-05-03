f1e66d878f4970479ab6250971b126dfa12a2174
Guilherme Rodrigues <gadr90@gmail.com>
2017-05-03 17:58:03 -0300

    Validate workspace name upon creation

--- src/modules/workspace/create.ts
+++ src/modules/workspace/create.ts
@@ -4,10 +4,18 @@ import log from '../../logger'
 import {getAccount} from '../../conf'
 import {workspaces} from '../../clients'
 
+const VALID_WORKSPACE = /^[a-z][a-z0-9-]{0,126}[a-z0-9]$/
+
 export default {
   requiredArgs: 'name',
   description: 'Create a new workspace with this name',
   handler: (name: string) => {
+    if (!VALID_WORKSPACE.test(name)) {
+      const err = new Error()
+      err.name = 'InterruptionError'
+      log.error('Whoops! That\'s not a valid workspace name. Please use only lowercase letters, numbers and hyphens.')
+      throw err
+    }
     log.debug('Creating workspace', name)
     return workspaces.create(getAccount(), name)
       .then(() => log.info(`Workspace ${chalk.green(name)} created successfully`))
