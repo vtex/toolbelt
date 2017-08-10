fceb44db4557479b9a9bfad64a827dc89870355e
Thor Amorim <thoramorim@gmail.com>
2017-08-10 18:27:46 -0300

    Fix local eslint handler file path

--- src/modules/tree.ts
+++ src/modules/tree.ts
@@ -219,7 +219,7 @@ export default {
           type: 'boolean',
         },
       ],
-      handler: './local/update',
+      handler: './local/eslint',
     },
     package: {
       description: 'Generate package.json from manifest',
