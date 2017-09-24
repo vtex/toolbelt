7b499dcc6f75a89ea0aa219a288ba4444c669c73
Mark Matern <mrkmat@gmail.com>
2017-09-24 16:55:25 -0300

    Wks list shows weight and production (#340)

--- src/modules/workspace/list.ts
+++ src/modules/workspace/list.ts
@@ -9,13 +9,15 @@ const [account, currentWorkspace] = [getAccount(), getWorkspace()]
 
 export default () => {
   log.debug('Listing workspaces')
-  const table = new Table({head: ['Name', 'Last Modified', 'State']})
+  const table = new Table({head: ['Name', 'Weight', 'Production']})
   return workspaces.list(account)
     .then((workspaces: WorkspaceResponse[]) =>
       workspaces.forEach(workspace => {
         const name = workspace.name === currentWorkspace
           ? chalk.green(`* ${workspace.name}`) : workspace.name
-        table.push([name, '?', '?'])
+        const weight = workspace.weight
+        const production = workspace.production
+        table.push([name, weight, production])
       }),
     )
     .then(() => console.log(table.toString()))
