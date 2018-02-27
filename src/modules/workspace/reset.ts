dacb78b6330ebe0bb766be8d225f865041c99d04
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-02-27 20:33:04 -0300

    Use new reset method from Workspaces client

--- src/modules/workspace/reset.ts
+++ src/modules/workspace/reset.ts
@@ -1,19 +1,45 @@
+import * as chalk from 'chalk'
+import * as inquirer from 'inquirer'
+import * as Bluebird from 'bluebird'
 import log from '../../logger'
-import {deleteWorkspaces} from './delete'
-import createCmd from './create'
-import {getWorkspace} from '../../conf'
+import {getAccount, getWorkspace} from '../../conf'
+import {workspaces} from '../../clients'
 
-export default async (name: string) => {
-  log.debug('Resetting workspace', name)
-  const workspace = name || getWorkspace()
-  try {
-    await deleteWorkspaces([workspace])
-    await createCmd(workspace)
-  } catch (err) {
-    if (err.response && err.response.data.code === 'WorkspaceAlreadyExists') {
-      setTimeout(() => createCmd(workspace), 3000)
-      return
+const promptWorkspaceReset = (name: string): Bluebird<void> =>
+  inquirer.prompt({
+    type: 'confirm',
+    name: 'confirm',
+    message: `Are you sure you want to reset workspace ${chalk.green(name)}?`,
+  })
+  .then(({confirm}) => {
+    if (!confirm) {
+      process.exit()
     }
+  })
+
+export default async (name: string, options) => {
+  const account = getAccount()
+  const workspace = name || getWorkspace()
+  const preConfirm = options.y || options.yes
+
+  log.debug('Resetting workspace', workspace)
+
+  if (!preConfirm) {
+    await promptWorkspaceReset(workspace)
+  }
+
+  try {
+    log.debug('Starting to reset workspace', workspace)
+    await workspaces.reset(account, workspace)
+    log.info(`Workspace ${chalk.green(workspace)} resetted ${chalk.green('successfully')}`)
+  } catch (err) {
+    log.warn(`Workspace ${chalk.green(workspace)} was ${chalk.red('not')} reseted`)
+    if (err.response) {
+      const {status, statusText, data = {message: null}} = err.response
+      const message = data.message || data
+      log.error(`Error ${status}: ${statusText}. ${message}`)
+    }
+
     throw err
   }
 }
