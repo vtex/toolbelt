766daf2e84c008cd827e906edf90aca5e036e397
José Iago Pereira Araújo <joseiago.p.a@gmail.com>
2018-06-11 23:30:44 -0300

    Remove option to publish multiple apps at the same time

--- src/modules/apps/publish.ts
+++ src/modules/apps/publish.ts
@@ -2,7 +2,7 @@ import * as Bluebird from 'bluebird'
 import { readFileSync } from 'fs-extra'
 import * as ora from 'ora'
 import { resolve } from 'path'
-import { forEach, map, prepend, prop } from 'ramda'
+import { map, prop } from 'ramda'
 
 import { BuildResult } from '@vtex/api'
 import chalk from 'chalk'
@@ -17,7 +17,7 @@ import switchAccount from '../auth/switch'
 import { listenBuild } from '../build'
 import { listLocalFiles } from './file'
 import { legacyPublisher } from './legacyPublish'
-import { parseArgs, pathToFileObject } from './utils'
+import { pathToFileObject } from './utils'
 
 
 const root = process.cwd()
@@ -55,52 +55,50 @@ const publisher = (workspace: string = 'master') => {
     return await builder.publishApp(appId, filesWithContent, tag)
   }
 
-  const publishApps = async (paths: string[], tag: string): Promise<void | never> => {
+  const publishApps = async (path: string, tag: string): Promise<void | never> => {
     const previousAccount = getAccount()
-    forEach(async (p: string) => {
-      const path = resolve(p)
-      const manifest = JSON.parse(readFileSync(resolve(path, 'manifest.json'), 'utf8'))
-      const account = getAccount()
+    
+    const manifest = JSON.parse(readFileSync(resolve(path, 'manifest.json'), 'utf8'))
+    const account = getAccount()
 
       if (manifest.vendor !== account) {
-        const switchToVendorMsg = `You are not logged in the vendor account for publishing the app. Wanna do that now going to ${chalk.blue(manifest.vendor)} account?`
-        const canSwitchToVendor = await promptPublishOnVendor(switchToVendorMsg)
-        if (!canSwitchToVendor) {
-          return
-        }
-        await switchAccount(manifest.vendor, {})
+      const switchToVendorMsg = `You are not logged in the vendor account for publishing the app. Wanna do that now going to ${chalk.blue(manifest.vendor)} account?`
+      const canSwitchToVendor = await promptPublishOnVendor(switchToVendorMsg)
+      if (!canSwitchToVendor) {
+        return
       }
+      await switchAccount(manifest.vendor, {})
+    }
 
-     const context = { account: manifest.vendor, workspace, region: region(), authToken: getToken() }
-     const { builder } = createClients(context, { timeout: 60000 })
+    const context = { account: manifest.vendor, workspace, region: region(), authToken: getToken() }
+    const { builder } = createClients(context, { timeout: 60000 })
 
-      const pubTag = tag || automaticTag(manifest.version)
+    const pubTag = tag || automaticTag(manifest.version)
 
-      if (manifest.builders.render
-        || manifest.builders['functions-ts']) {
-        const unlisten = logAll({ account, workspace }, log.level, `${manifest.vendor}.${manifest.name}`)
-        const { legacyPublishApp } = legacyPublisher(manifest.vendor, workspace)
-        await legacyPublishApp(path, pubTag, manifest).finally(unlisten)
-      } else {
-        const appId = toAppLocator(manifest)
-        const oraMessage = ora(`Publishing ${appId} ...`)
-        const spinner = log.level === 'debug' ? oraMessage.info() : oraMessage.start()
-        try {
-          const { response } = await listenBuild(appId, () => publishApp(path, appId, pubTag, builder), { waitCompletion: true, context })
-          if (response.code !== 'build.accepted') {
-            spinner.warn(`${appId} was published successfully, but you should update your builder hub to the latest version.`)
-          } else {
-             spinner.succeed(`${appId} was published successfully!`)
-          }
-        } catch (e) {
-          spinner.fail(`Fail to publish ${appId}`)
-          log.error(e.message)
-          await switchToPreviousAccount(previousAccount)
-          throw e
+    if (manifest.builders.render
+      || manifest.builders['functions-ts']) {
+      const unlisten = logAll({ account, workspace }, log.level, `${manifest.vendor}.${manifest.name}`)
+      const { legacyPublishApp } = legacyPublisher(manifest.vendor, workspace)
+      await legacyPublishApp(path, pubTag, manifest).finally(unlisten)
+    } else {
+      const appId = toAppLocator(manifest)
+      const oraMessage = ora(`Publishing ${appId} ...`)
+      const spinner = log.level === 'debug' ? oraMessage.info() : oraMessage.start()
+      try {
+        const { response } = await listenBuild(appId, () => publishApp(path, appId, pubTag, builder), { waitCompletion: true, context })
+        if (response.code !== 'build.accepted') {
+          spinner.warn(`${appId} was published successfully, but you should update your builder hub to the latest version.`)
+        } else {
+            spinner.succeed(`${appId} was published successfully!`)
         }
+      } catch (e) {
+        spinner.fail(`Fail to publish ${appId}`)
+        log.error(e.message)
+        await switchToPreviousAccount(previousAccount)
+        throw e
       }
-      await switchToPreviousAccount(previousAccount)
-    }, paths)
+    }
+    await switchToPreviousAccount(previousAccount)
     
     Promise.resolve()
   }
@@ -115,8 +113,8 @@ export default (path: string, options) => {
 
   log.debug(`Starting to publish app in ${getEnvironment()}`)
 
-  const paths = prepend(path || root, parseArgs(options._))
+  path = path || root
   const workspace = options.w || options.workspace
   const { publishApps } = publisher(workspace)
-  return publishApps(paths, options.tag)
+  return publishApps(path, options.tag)
 }
