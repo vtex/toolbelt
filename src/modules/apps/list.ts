4fbc56657c9ab8b67dcd6ca49e9ffd0282fa016a
Lucas Bebber <lucasbbebber@gmail.com>
2019-01-17 16:43:20 -0200

    Add --short option to list command

--- src/modules/apps/list.ts
+++ src/modules/apps/list.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk'
 import * as Table from 'cli-table'
-import { compose, curry, flip, gt, head, length, map, prop, split } from 'ramda'
+import { compose, flip, gt, head, length, map, prop, split } from 'ramda'
 
 import { apps } from '../../clients'
 import { getAccount, getWorkspace } from '../../conf'
@@ -20,28 +20,56 @@ const cleanVersion =
 const isLinked =
   compose<string, string[], number, boolean>(flippedGt(1), length, split('+build'))
 
-const renderTable = curry<string, string, any, void>(
-  (title: string, emptyMessage: string, appArray): void => {
+const renderTable = (
+  ({ title, emptyMessage, short, appArray }: {
+    title: string,
+    emptyMessage: string,
+    short: boolean,
+    appArray: any,
+  }): void => {
     console.log(title)
     if (appArray.length === 0) {
       return console.log(`${emptyMessage}\n`)
     }
-    const table = new Table({ head: ['Vendor', 'Name', 'Version', 'Linked'] })
-    appArray.forEach(({ vendor, name, version }) => {
-      const linked = isLinked(version) ? chalk.green('yes') : 'no'
-      const cleanedVersion = cleanVersion(version)
-      table.push([vendor, name, cleanedVersion, linked])
+    const table = new Table({
+      head: !short && ['Vendor', 'Name', 'Version', 'Linked'],
+      ...(short && {
+        chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+          , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+          , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+          , 'right': '' , 'right-mid': '' , 'middle': '   ' },
+        style: { 'padding-left': 0, 'padding-right': 0 }
+      }),
     })
+
+    appArray.forEach(({ vendor, name, version }) => {
+      const linkedLabel = short
+        ? isLinked(version) ? chalk.green('linked') : 'not linked'
+        : isLinked(version) ? chalk.green('yes') : 'no'
+
+      const cleanedVersion = cleanVersion(version)
+
+      table.push(short
+        ? [`${chalk.black(vendor)}.${name}`, cleanedVersion, linkedLabel]
+        : [vendor, name, cleanedVersion, linkedLabel]
+      )
+    })
+
     console.log(`${table.toString()}\n`)
   }
 )
 
-export default () => {
+export default ({ s, short }) => {
   const account = getAccount()
   const workspace = getWorkspace()
   log.debug('Starting to list apps')
   return listApps()
     .then(prop('data'))
     .then(parseLocatorFromList)
-    .then(renderTable(`${chalk.green('Installed Apps')} in ${chalk.blue(account)} at workspace ${chalk.green(workspace)}`, 'You have no installed apps'))
+    .then(appArray => renderTable({
+      title: `${chalk.green('Installed Apps')} in ${chalk.blue(account)} at workspace ${chalk.green(workspace)}`,
+      emptyMessage: 'You have no installed apps',
+      short: !!(s || short),
+      appArray,
+    }))
 }
