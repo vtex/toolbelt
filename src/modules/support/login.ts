0b704254f4330d11f649c1c3f19f29233e55dea5
Pedro Pereira <grey.pedrinho@gmail.com>
2019-01-23 19:14:45 -0200

    Adds prompt of available roles

--- src/modules/support/login.ts
+++ src/modules/support/login.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
+import * as inquirer from 'inquirer'
+import { prop } from 'ramda'
 import { getAccount, getToken, getWorkspace } from '../../conf'
 import * as env from '../../env'
+
 
 
 const getAvailableRoles = async (region: string, token: string, supportedAccount: string): Promise<string[]> => {
@@ -15,14 +18,29 @@ const getAvailableRoles = async (region: string, token: string, supportedAccount
   return response.data
 }
 
+const promptRoles = async (roles: string[]): Promise<string> => {
+  const cancel = 'Cancel'
+  const chosen = prop<string>('role', await inquirer.prompt({
+    name: 'role',
+    message: 'Which role do you want to assume?',
+    type: 'list',
+    choices: [...roles, cancel],
+  }))
+  if (chosen === cancel) {
+    console.info('Bye! o/')
+    return process.exit()
+  }
+  return chosen
+}
+
 export default async ({ a, account, _ }) => {
   const supportedAccount = account || a || (_ && _[0])
   const actualToken = getToken()
   const region = env.region()
   try {
     const roles = await getAvailableRoles(region, actualToken, supportedAccount)
-
-    console.log({ roles })
+    const role = await promptRoles(roles)
+    console.log({ role })
   }
   catch (err) {
     console.log({ err })
