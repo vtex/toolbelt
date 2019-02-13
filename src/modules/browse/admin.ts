b75158189cd49abd423618e8caec1626644e1556
Pedro Pereira <grey.pedrinho@gmail.com>
2019-02-13 12:00:43 -0200

    Implements changes suggested by @igorframos

--- src/modules/browse/admin.ts
+++ src/modules/browse/admin.ts
@@ -1,24 +1,15 @@
 import * as opn from 'opn'
 import * as conf from '../../conf'
 
-const buildPrefix = (account: string, workspace: string): string => {
-  const base = 'https://'
-  const workspacePrefix = workspace === 'master' ? '' : `${workspace}--`
-  return base + workspacePrefix + account
-}
-
-const buildDomain = (region: conf.Environment): string =>
+const chooseDomain = (region: conf.Environment): string =>
   region === conf.Environment.Production
-    ? '.myvtex.com'
-    : '.myvtexdev.com'
+    ? 'myvtex.com'
+    : 'myvtexdev.com'
 
 export default () => {
   const region = conf.getEnvironment()
   const { account, workspace } = conf.currentContext
-  const prefix = buildPrefix(account, workspace)
-  const domain = buildDomain(region)
-  const path = '/admin'
-  const uri = prefix + domain + path
+  const uri = `https://${workspace}--${account}.${chooseDomain(region)}/admin`
 
   opn(uri, { wait: false })
 }
