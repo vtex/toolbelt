c87bade9abce279fc8f6ee0172e81b4aea5bb0cc
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-02-19 18:21:54 -0300

    Implement string reducer function

--- src/modules/deps/utils.ts
+++ src/modules/deps/utils.ts
@@ -26,12 +26,59 @@ return await new Apps(context)
   .then(cleanDeps)
 }
 
+const stringReducer = (s:string, maxLength: number) => {
+  /* The most general app description is as follows:
+   * {vendor}.{appName}@{version}+build{buildNumber}
+   *
+   * This function abbreviates this description so that it does not exceed the
+   * maxLength parameter, but always shows the full version and the beginning
+   * part of the app name.
+   */
+  if (s.length <= maxLength) {
+    return s
+  }
+  // If s.length > maxLength we need to reduce the string's size,
+  // but still keep the useful information.
+  const hideString = '..'
+  const versionPrefix = '@'
+  const buildPrefix = '+build'
+  // First try to remove build number:
+  if (s.indexOf(buildPrefix) > -1) {
+    if (`${s.split(buildPrefix)[0]}${buildPrefix}${hideString}`.length <= maxLength) {
+      return `${s.substring(0, maxLength-hideString.length)}${hideString}`
+    }
+  }
+  // First try didn't work, so do some more elaborated work.
+  // We will abbreviate the both the app name and the build number
+  // (if it exists).
+  const [appName, version] = s.split(versionPrefix)
+  let pureVersion
+  let additionalLength
+  if (version.indexOf(buildPrefix) > -1) {
+    // There is a build number, so we will need to abbreviate it as well.
+    pureVersion = version.split(buildPrefix)[0]
+    additionalLength = buildPrefix.length + hideString.length
+  } else {
+    // No build number.
+    pureVersion = version
+    additionalLength = 0
+  }
+  const appNameDesiredLength = (maxLength - hideString.length -
+    pureVersion.length - versionPrefix.length - additionalLength)
+  const reducedAppName = `${appName.substr(0, appNameDesiredLength)}${hideString}`
+  const stringWithReducedName = `${reducedAppName}${versionPrefix}${version}`
+  const finalString =
+    `${stringWithReducedName.substring(0, maxLength-hideString.length)}${hideString}`
+  return finalString
+}
+
 export const matchedDepsDiffTable = (
   title1: string,
   title2: string,
   deps1: string[],
   deps2: string[]
 ) => {
+  const colWidth = 40 // column width of the table.
   const depsDiff = diffArrays(deps1, deps2)
   // Get deduplicated names (no version) of the changed deps.
   const depNames = [...new Set(
@@ -63,13 +110,13 @@ export const matchedDepsDiffTable = (
   // Setting the objects values.
   setObjectValues(
     removedDeps,
-    (k) => chalk.red(`-${k}`),
+    (k) => chalk.red(stringReducer(`-${k}`, colWidth)),
     'right',
     (k:any) => !!k.removed
   )
   setObjectValues(
     addedDeps,
-    (k) => chalk.green(`+${k}`),
+    (k) => chalk.green(stringReducer(`+${k}`, colWidth)),
     'left',
     (k:any) => !!k.added
   )
