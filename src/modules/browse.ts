f8e0e2299ce152280059652f28d4b270d0167a27
Igor Ramos <igor.ramos@vtex.com.br>
2019-05-24 20:16:01 -0300

    Move functionality from conflicting command
    `vtex browse admin` was an awesome idea that got incorporated into toolbelt
    before we got to merge our code. The master branch now has a more general
    version of the command (`vtex browse`) and it conflicted with our
    implementation. The solution is to incorporate our support functionality to the
    code that is already in master and get rid of our own command.

--- src/modules/browse.ts
+++ src/modules/browse.ts
@@ -1,11 +1,44 @@
+import axios from 'axios'
+import * as jwt from 'jsonwebtoken'
 import * as opn from 'opn'
+import * as R from 'ramda'
 import * as conf from '../conf'
 import { clusterIdDomainInfix, publicEndpoint } from '../env'
 // Doesn't seem to work with 'import', seems to return undefined for some reason ¯\_(ツ)_/¯
 const QRCode = require('qrcode-terminal') // tslint:disable-line no-var-requires
 
-export default (endpoint='', {q, qr}) => {
+
+const isSupportRole = (role: string): boolean => role && role.startsWith('vtex.support-authority')
+
+const isSupportSession = (): boolean => {
+  const token = conf.getToken()
+  const decoded = jwt.decode(token)
+  if (!decoded || typeof decoded === 'string') {
+    return false
+  }
+
+  return R.any(role => isSupportRole(role), decoded.roles as string[])
+}
+
+const prepareSupportBrowser = async (account: string, workspace: string): Promise<string> => {
+  const token = conf.getToken()
+
+  const uri = `https://${workspace}--${account}.${publicEndpoint()}/_v/private/support-login/prepare`
+  const response = await axios.get(uri,
+    {
+      headers: {
+        'X-Vtex-Original-Credential': token,
+      },
+    })
+  return response.data.oneTimeToken
+}
+
+export default async (endpoint='', {q, qr}) => {
   const { account, workspace } = conf.currentContext
+  if (isSupportSession()) {
+    const token = await prepareSupportBrowser(account, workspace)
+    endpoint = `_v/private/support-login/login?token=${token}&returnUrl=/${endpoint}`
+  }
   const uri = `https://${workspace}--${account}${clusterIdDomainInfix()}.${publicEndpoint()}/${endpoint}`
 
   if (q || qr) {
