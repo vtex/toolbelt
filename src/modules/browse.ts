74aef914e3104e49971f9a3915effee4ed9f5bb9
Lucas Bebber <lucasbbebber@gmail.com>
2019-05-25 15:33:35 -0300

    Output QR Code on terminal if the flag qr is on

--- src/modules/browse.ts
+++ src/modules/browse.ts
@@ -1,10 +1,16 @@
 import * as opn from 'opn'
 import * as conf from '../conf'
 import { clusterIdDomainInfix, publicEndpoint } from '../env'
+const QRCode = require('qrcode-terminal')
 
-export default (endpoint='') => {
+export default (endpoint='', {q, qr}) => {
   const { account, workspace } = conf.currentContext
   const uri = `https://${workspace}--${account}${clusterIdDomainInfix()}.${publicEndpoint()}/${endpoint}`
+
+  if (q || qr) {
+    QRCode.generate(uri, { small: true })
+    return
+  }
 
   opn(uri, { wait: false })
 }
