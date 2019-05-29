373bb58edfd04e5171319748586d6010a768bea9
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-05-29 19:47:03 -0300

    Change tables order

--- src/modules/apps/list.ts
+++ src/modules/apps/list.ts
@@ -12,6 +12,10 @@ const { listApps } = apps
 const cleanVersion =
   compose<string, string[], string>(head, split('+build'))
 
+const filterBySource = (source: string) =>
+  // More recent Apps versions return app list with a `_source` field.
+  filter(compose<any, string, boolean>(equals(source), prop('_source')))
+
 const renderTable = (
   ({ title, emptyMessage, appArray }: {
     title: string,
@@ -47,19 +51,19 @@ export default async () => {
   log.debug('Starting to list apps')
   const appArray = await listApps()
     .then(prop('data'))
+  renderTable({  // Apps inherited by account's edition
+    title: `${chalk.yellow('Edition Apps')} in ${chalk.blue(account)} at workspace ${chalk.yellow(workspace)}`,
+    emptyMessage: 'You have no edition apps',
+    appArray: filterBySource('edition')(appArray),
+  })
   renderTable({  // Installed apps
     title: `${chalk.yellow('Installed Apps')} in ${chalk.blue(account)} at workspace ${chalk.yellow(workspace)}`,
     emptyMessage: 'You have no installed apps',
-    appArray: filter(compose<any, string, boolean>(equals('installation'), prop('_source')))(appArray),
+    appArray: filterBySource('installation')(appArray),
   })
   renderTable({  // Linked apps
     title: `${chalk.yellow('Linked Apps')} in ${chalk.blue(account)} at workspace ${chalk.yellow(workspace)}`,
     emptyMessage: 'You have no linked apps',
-    appArray: filter(compose<any, string, boolean>(equals('link'), prop('_source')))(appArray),
-  })
-  renderTable({  // Apps inherited by account's edition
-    title: `${chalk.yellow('Edition Apps')} in ${chalk.blue(account)} at workspace ${chalk.yellow(workspace)}`,
-    emptyMessage: 'You have no edition apps',
-    appArray: filter(compose<any, string, boolean>(equals('edition'), prop('_source')))(appArray),
+    appArray: filterBySource('link')(appArray),
   })
 }
