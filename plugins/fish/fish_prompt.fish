2c825490e4f624ee427db26aee73ab0d881651f2
Victor Colombo <victorcolombo@gmail.com>
2019-06-17 16:40:11 -0300

    Staging is dead

--- plugins/fish/fish_prompt.fish
+++ plugins/fish/fish_prompt.fish
@@ -6,17 +6,13 @@ function get_vtex_account
   parse_vtex_json account
 end
 
-function get_vtex_env
-  parse_vtex_json env
-end
-
 function get_vtex_workspace
   parse_vtex_json workspace
 end
 
 function prompt_vtex
   if test (get_vtex_workspace 2> /dev/null)
-    echo (get_vtex_env):(get_vtex_account)/(get_vtex_workspace)
+    echo (get_vtex_account)/(get_vtex_workspace)
   end
 end
 
