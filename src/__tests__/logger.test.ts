5e666c46348b6faa51baf1c10e1aca24faf15609
Tiago Nápoli <napoli.tiago96@gmail.com>
2019-08-09 15:02:15 -0300

    Migrate existing tests to jest testing framework (#601)
    * Use jest instead of ava. Remove coveralls, nyc dependencies
    
    * Add jest.config.js
    
    * Remove .nyc_output from .gitignore. Add coverage folder
    
    * Add jest types to tsconfig
    
    * Migrate tests to jest
    
    * Exclude __tests__ from compilation result
    
    * Update CHANGELOG

---
+++ src/__tests__/logger.test.ts
@@ -0,0 +1,5 @@
+import winston from '../logger'
+
+test('winston is exported', () => {
+  expect(winston).toBeTruthy()
+})
---
+++ src/__tests__/modules/apps/file.test.ts
@@ -0,0 +1,15 @@
+import * as path from 'path'
+import { listLocalFiles } from '../../../modules/apps/file'
+
+const root = path.resolve(__dirname, '../../fixtures/file-test')
+
+test('list files in a directory ignoring everything except the folders and the manifest file', async () => {
+  const expectedFiles = [
+    'manifest.json',
+    'render/Foo.js',
+    'render/style.scss',
+    // 'empty' is an empty file and therefore is ignored.
+  ]
+  const files = await listLocalFiles(root)
+  expect(files).toStrictEqual(expectedFiles)
+})
