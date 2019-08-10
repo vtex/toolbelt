eb903eaa74d3b30fb70a45ff0950673238ce6506
tiagonapoli <napoli.tiago96@gmail.com>
2019-08-09 21:58:03 -0300

    Update winston

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,13 @@
 import * as winston from 'winston'
+import { isVerbose } from './utils'
 
-// TODO: Configure transport to send errors to Splunk
-winston.cli()
+const logger = winston.createLogger({
+  level: 'debug',
+  transports: [
+    new winston.transports.Console({
+      level: isVerbose ? 'debug' : 'info',
+    }),
+  ],
+})
 
-export default winston
+export default logger
