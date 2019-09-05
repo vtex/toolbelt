c51b9bd8179f3853304fddcb9695b4f044d62e39
Breno Calazans <brenocalazans@gmail.com>
2019-09-05 16:12:00 -0300

    Update notifier now show yarn command

--- src/update.ts
+++ src/update.ts
@@ -3,5 +3,6 @@ import * as updateNotifier from 'update-notifier'
 import * as pkg from '../package.json'
 
 export default function notify() {
-  updateNotifier({ pkg, updateCheckInterval: 1000 * 60 * 60 * 1 }).notify({ isGlobal: true })
+  updateNotifier({ pkg, updateCheckInterval: 1000 * 60 * 60 * 1 })
+    .notify({ isGlobal: true, isYarnGlobal: true })
 }
