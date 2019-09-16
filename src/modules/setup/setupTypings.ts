0dffae51da3c9677d185e28a350985cd1a5338f6
tiagonapoli <tiago.napoli@vtex.com.br>
2019-09-16 11:59:45 -0300

    Add ignoreLinked option to setupTypings. Check if new typings url is available before adding types url

--- src/modules/setup/setupTypings.ts
+++ src/modules/setup/setupTypings.ts
@@ -1,33 +1,49 @@
+import chalk from 'chalk'
 import * as R from 'ramda'
 import { createClients } from '../../clients'
 import { getAccount, getWorkspace } from '../../conf'
 import { publicEndpoint } from '../../env'
+import { toMajorRange } from '../../locator'
 import log from '../../logger'
-import { isLinked, resolveAppId } from '../apps/utils'
+import { isLinked, resolveAppId, appIdFromRegistry } from '../apps/utils'
 import { runYarn } from '../utils'
-import { packageJsonEditor } from './utils'
+import { checkIfTarGzIsEmpty, packageJsonEditor } from './utils'
 
 const getVendor = (appId: string) => appId.split('.')[0]
-
-const typingsPath = 'public/_types'
 const typingsURLRegex = /_v\/\w*\/typings/
 
-const appTypingsURL = async (appName: string, appVersion: string, builder: string): Promise<string> => {
-  const appId = await resolveAppId(appName, appVersion)
+const appTypingsURL = async (appName: string, appMajorLocator: string, ignoreLinked: boolean): Promise<string> => {
+  const appId = ignoreLinked
+    ? await appIdFromRegistry(appName, appMajorLocator)
+    : await resolveAppId(appName, appMajorLocator)
   const vendor = getVendor(appId)
-  if (isLinked({ version: appId, vendor, name: '', builders: {} })) {
-    return `https://${getWorkspace()}--${getAccount()}.${publicEndpoint()}/_v/private/typings/linked/v1/${appId}/${typingsPath}/${builder}`
+  const linked = isLinked({ version: appId, vendor, name: '', builders: {} })
+
+  const oldSuffix = `/_types/react`
+  const newSuffix = `/@types/${appName}`
+
+  const base =
+    linked && !ignoreLinked
+      ? `https://${getWorkspace()}--${getAccount()}.${publicEndpoint()}/_v/private/typings/linked/v1/${appId}/public`
+      : `http://vtex.vteximg.com.br/_v/public/typings/v1/${appId}/public`
+
+  log.info(`Checking if ${chalk.bold(appId)} has new types format`)
+  try {
+    const newTypesExist = !(await checkIfTarGzIsEmpty(base + newSuffix))
+    return base + (newTypesExist ? newSuffix : oldSuffix)
+  } catch (err) {
+    log.error(`Error checking if types package is empty for ${base + newSuffix}`)
+    throw err
   }
-  return `http://${vendor}.vteximg.com.br/_v/public/typings/v1/${appId}/${typingsPath}/${builder}`
 }
 
-const appsWithTypingsURLs = async (builder: string, appDependencies: Record<string, any>) => {
+const appsWithTypingsURLs = async (appDependencies: Record<string, any>, ignoreLinked: boolean) => {
   const result: Record<string, any> = {}
   const appNamesAndDependencies = R.toPairs(appDependencies)
   await Promise.all(
     appNamesAndDependencies.map(async ([appName, appVersion]: [string, string]) => {
       try {
-        result[appName] = await appTypingsURL(appName, appVersion, builder)
+        result[appName] = await appTypingsURL(appName, appVersion, ignoreLinked)
       } catch (e) {
         log.error(`Unable to generate typings URL for ${appName}@${appVersion}.`)
       }
@@ -37,15 +53,21 @@ const appsWithTypingsURLs = async (builder: string, appDependencies: Record<stri
   return result
 }
 
-const getDepsInjectedByBuilderHub = (typingsData: any, version: string, builder: string) => {
+const getBuilderDependencies = (
+  manifestDependencies: Pick<Manifest, 'dependencies'>,
+  typingsData: any,
+  version: string,
+  builder: string
+) => {
   const builderTypingsData = R.prop(builder, typingsData)
+  let injectedDependencies = {}
   if (builderTypingsData && R.has(version, builderTypingsData)) {
-    return R.path([version, 'injectedDependencies'], builderTypingsData)
+    injectedDependencies = R.path([version, 'injectedDependencies'], builderTypingsData)
   }
-  return null
+  return R.merge(manifestDependencies, injectedDependencies)
 }
 
-const injectTypingsInPackageJson = async (appDeps: Record<string, any>, builder: string) => {
+const injectTypingsInPackageJson = async (appDeps: Record<string, any>, ignoreLinked: boolean, builder: string) => {
   let packageJson
   try {
     packageJson = packageJsonEditor.read(builder)
@@ -59,7 +81,7 @@ const injectTypingsInPackageJson = async (appDeps: Record<string, any>, builder:
   log.info(`Injecting typings on ${builder}'s package.json`)
   const oldDevDeps = packageJson.devDependencies || {}
   const oldTypingsEntries = R.filter(R.test(typingsURLRegex), oldDevDeps)
-  const newTypingsEntries = await appsWithTypingsURLs(builder, appDeps)
+  const newTypingsEntries = await appsWithTypingsURLs(appDeps, ignoreLinked)
   if (!R.equals(oldTypingsEntries, newTypingsEntries)) {
     const cleanOldDevDeps = R.reject(R.test(typingsURLRegex), oldDevDeps)
     packageJsonEditor.write(builder, {
@@ -75,26 +97,27 @@ const injectTypingsInPackageJson = async (appDeps: Record<string, any>, builder:
   }
 }
 
-export const setupTypings = async (manifest: Manifest) => {
-  const { builder: builderClient } = createClients({}, { retries: 2 })
+export const setupTypings = async (manifest: Manifest, ignoreLinked: boolean, buildersToAddTypes: string[]) => {
+  const appName = manifest.vendor + '.' + manifest.name
+  const appMajor = toMajorRange(manifest.version)
+
+  const { builder: builderClient } = createClients({}, { retries: 2, timeout: 2000 })
+  const builders = R.keys(R.prop('builders', manifest) || {})
+  const filteredBuilders = R.intersection(builders, buildersToAddTypes)
 
   log.info('Fetching names of dependencies injected by BuilderHub')
   const typingsData = await builderClient.typingsInfo()
-  const buildersWithInjectedDeps = R.pipe(
-    R.prop('builders'),
-    R.mapObjIndexed(R.curry(getDepsInjectedByBuilderHub)(typingsData)),
-    R.reject(R.isNil)
-  )(manifest)
-
-  const buildersWithAppDepsAndInjectedDeps = R.pipe(
-    R.mapObjIndexed(injectedDependencies => R.merge(manifest.dependencies, injectedDependencies)),
-    R.reject(R.isEmpty)
-  )(buildersWithInjectedDeps as Record<string, any>)
+  const buildersWithAllDeps = filteredBuilders.map((builder: string) => {
+    return {
+      builder,
+      deps: {
+        ...getBuilderDependencies(manifest.dependencies, typingsData, manifest.builders[builder], builder),
+        ...(builder === 'node' ? { [appName]: appMajor } : {}),
+      },
+    }
+  })
 
   await Promise.all(
-    R.pipe(
-      R.mapObjIndexed(injectTypingsInPackageJson),
-      R.values
-    )(buildersWithAppDepsAndInjectedDeps as Record<string, any>)
+    buildersWithAllDeps.map(({ builder, deps }) => injectTypingsInPackageJson(deps, ignoreLinked, builder))
   )
 }
