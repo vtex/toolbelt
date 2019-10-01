918ec1d475bafbc334f063259ec20b4976e24ac9
Thiago <thiago.tergolino@vtex.com>
2019-10-01 19:03:16 -0300

    Fix message in enquirer to prompt proportionOfTraffic

--- src/modules/workspace/abtest/utils.ts
+++ src/modules/workspace/abtest/utils.ts
@@ -114,7 +114,9 @@ export const promptProportionTrafic = async (): Promise<string> => {
     'proportion',
     await enquirer.prompt({
       name: 'proportion',
-      message: `What's the proportion of traffic directed to workspace ${chalk.blue('master')}? This should be an integer between 0 and 10000 that corresponds each 1% to 100, i.e. if you want to direct 54.32% of traffic to master, this value should be 5432.`,
+      message: `What's the proportion of traffic initially directed to workspace ${chalk.blue('master')}?
+      This should be an integer between 0 and 10000 that corresponds each 1% to 100, i.e. if you want to direct 54.32% of traffic to master, this value should be 5432.
+      If you don't want to fix this value, just type any value here and set the next restriction to 0.`,
       validate: s => /^([0-9]{1,4}|10000)$/.test(s) || message,
       filter: s => s.trim(),
       type: 'input',
