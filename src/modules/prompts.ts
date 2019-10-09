70c9781dc63fd8b3659fac1dd29c354f8ceccc56
tiagonapoli <tiago.napoli@vtex.com.br>
2019-10-09 19:32:27 -0300

    Set process.stdin tty before and after prompting confirm

--- src/modules/prompts.ts
+++ src/modules/prompts.ts
@@ -2,5 +2,11 @@ import * as Confirm from 'prompt-confirm'
 
 // Prompt definitions that are used in many modules of this project.
 
-export const promptConfirm = async (message: string, initial = true): Promise<boolean> =>
-  new Confirm({ message, default: initial }).run()
+export const promptConfirm = async (message: string, initial = true): Promise<boolean> => {
+  const initialMode = process.stdin.isRaw
+  const isStdinTTY = process.stdin.isTTY
+  if (isStdinTTY) process.stdin.setRawMode(true)
+  const ret = await new Confirm({ message, default: initial }).run()
+  if (isStdinTTY) process.stdin.setRawMode(initialMode)
+  return ret
+}
