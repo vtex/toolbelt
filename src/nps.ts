098dd281cf79b60da46658cc8589fc8b8b3a4c5b
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-12-02 14:57:17 -0300

    Format

--- src/nps.ts
+++ src/nps.ts
@@ -1,7 +1,7 @@
-import * as opn from 'opn'
 import * as enquirer from 'enquirer'
-import { prop, keys } from 'ramda'
 import * as moment from 'moment'
+import * as opn from 'opn'
+import { prop, keys } from 'ramda'
 
 import { getNextFeedbackDate, saveNextFeedbackDate } from './conf'
 import { promptConfirm } from './modules/prompts'
@@ -13,17 +13,22 @@ const choices = {
   'In 1 day': [1, 'days'],
   'In 1 week': [1, 'weeks'],
   'In 1 month': [1, 'months'],
-  'Never': NEVER,
+  Never: NEVER,
 }
 
-export default async function checkAndOpenNPSLink() { const nextFeedbackDateString = getNextFeedbackDate()
+export default async function checkAndOpenNPSLink() {
+  const nextFeedbackDateString = getNextFeedbackDate()
 
   if (nextFeedbackDateString === NEVER) {
     return
   }
   if (!nextFeedbackDateString) {
     // If the user is starting to use the tool, wait 1 week to ask for feedback.
-    saveNextFeedbackDate(moment().add(1, 'weeks').toISOString())
+    saveNextFeedbackDate(
+      moment()
+        .add(1, 'weeks')
+        .toISOString()
+    )
     return
   }
 
@@ -33,19 +38,27 @@ export default async function checkAndOpenNPSLink() { const nextFeedbackDateStri
     const shouldOpenFeedbackForm = await promptConfirm(`Would you please fill in the feedback form?`, false)
     if (shouldOpenFeedbackForm) {
       // Ask for feedback again in 3 months
-      saveNextFeedbackDate(moment().add(3, 'months').toISOString())
+      saveNextFeedbackDate(
+        moment()
+          .add(3, 'months')
+          .toISOString()
+      )
       opn(NPSFormURL, { wait: false })
     } else {
-      let remindChoice = await enquirer.prompt({
+      let remindChoice = await enquirer
+        .prompt({
           name: 'choice',
           message: 'When would you like to be reminded?',
           type: 'select',
           choices: keys(choices),
-        }).then(prop('choice'))
+        })
+        .then(prop('choice'))
 
       if (remindChoice !== NEVER) {
         const [n, unit] = choices[remindChoice]
-        remindChoice = moment().add(n, unit).toISOString()
+        remindChoice = moment()
+          .add(n, unit)
+          .toISOString()
       }
       saveNextFeedbackDate(remindChoice)
     }
