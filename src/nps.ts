844a098fd441efbe432a955821f193b0c52e7eb2
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-12-16 15:52:20 -0300

    Minor adjustments

--- src/nps.ts
+++ src/nps.ts
@@ -5,22 +5,18 @@ import * as opn from 'opn'
 import { getNextFeedbackDate, saveNextFeedbackDate } from './conf'
 import { promptConfirm } from './modules/prompts'
 
-const NEVER = 'Never'
 const NPSFormURL = 'https://forms.gle/CRRHn6P3x9AeaWTQ8'
 
 const choices = {
   'In 1 day': [1, 'days'],
   'In 1 week': [1, 'weeks'],
   'In 1 month': [1, 'months'],
-  [NEVER]: NEVER,
+  Never: [3, 'months'],
 }
 
 export async function checkAndOpenNPSLink() {
   const nextFeedbackDateString = getNextFeedbackDate()
 
-  if (nextFeedbackDateString === NEVER) {
-    return
-  }
   if (!nextFeedbackDateString) {
     // If the user is starting to use the tool, wait 1 week to ask for feedback.
     saveNextFeedbackDate(
@@ -34,7 +30,10 @@ export async function checkAndOpenNPSLink() {
   const nextFeedbackDate = moment(nextFeedbackDateString)
 
   if (moment() > nextFeedbackDate) {
-    const shouldOpenFeedbackForm = await promptConfirm(`Would you please fill in the feedback form?`, false)
+    const shouldOpenFeedbackForm = await promptConfirm(
+      `Help us evolve VTEX IO! Can you fill in our feedback form?`,
+      true
+    )
     if (shouldOpenFeedbackForm) {
       // Ask for feedback again in 3 months.
       saveNextFeedbackDate(
@@ -44,20 +43,17 @@ export async function checkAndOpenNPSLink() {
       )
       opn(NPSFormURL, { wait: false })
     } else {
-      let { remindChoice } = await enquirer
-        .prompt({
-          name: 'remindChoice',
-          message: 'When would you like to be reminded?',
-          type: 'select',
-          choices: Object.keys(choices),
-        })
+      let { remindChoice } = await enquirer.prompt({
+        name: 'remindChoice',
+        message: 'When would you like to be reminded?',
+        type: 'select',
+        choices: Object.keys(choices),
+      })
 
-      if (remindChoice !== NEVER) {
-        const [n, unit] = choices[remindChoice]
-        remindChoice = moment()
-          .add(n, unit)
-          .toISOString()
-      }
+      const [n, unit] = choices[remindChoice]
+      remindChoice = moment()
+        .add(n, unit)
+        .toISOString()
       saveNextFeedbackDate(remindChoice)
     }
   }
