5c114139d2c2d43fb35464d69d245ca31fd07fa6
Alexandre Nicastro <kryndex@gmail.com>
2019-12-23 13:58:48 -0300

    updateNotifier: add changelog url & improvements
    Not the best way to do it since we're adding a lot of technical debt.
    
    It'd be better to create an issue to change the module when possible.
    
    Fixes #677

--- src/update.ts
+++ src/update.ts
@@ -1,7 +1,35 @@
+import chalk from 'chalk'
 import * as updateNotifier from 'update-notifier'
 
 import * as pkg from '../package.json'
 
+const notifier = updateNotifier({ pkg, updateCheckInterval: 1000 * 60 * 60 * 1 })
+
 export default function notify() {
-  updateNotifier({ pkg, updateCheckInterval: 1000 * 60 * 60 * 1 }).notify({ isGlobal: true, isYarnGlobal: true })
+  if (notifier.update && notifier.update.latest !== pkg.version) {
+    const oldVersion = notifier.update.current
+    const latestVersion = notifier.update.latest
+    const changelog = `https://github.com/vtex/toolbelt/releases/tag/v${latestVersion}`
+    let type = notifier.update.type
+
+    switch (type) {
+      case 'major':
+        type = chalk.red(type)
+        break
+      case 'minor':
+        type = chalk.yellow(type)
+        break
+      case 'patch':
+        type = chalk.green(type)
+        break
+    }
+
+    notifier.notify({
+      isGlobal: true,
+      isYarnGlobal: true,
+      message: `New ${type} version of ${pkg.name} available! ${chalk.dim(oldVersion)} → ${chalk.green(latestVersion)}\n
+        ${chalk.yellow('Changelog:')} ${chalk.cyan(changelog)}\n
+        Run ${chalk.green(`yarn global add ${pkg.name}`)} to update!`,
+    })
+  }
 }
