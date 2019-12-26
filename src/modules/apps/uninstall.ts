3787adaed55e03137d76187b7cfd90f93f989079
tiagonapoli <tiago.napoli@vtex.com.br>
2019-12-26 17:59:37 -0300

    Refactor vtex uninstall

--- src/modules/apps/uninstall.ts
+++ src/modules/apps/uninstall.ts
@@ -1,13 +1,10 @@
 import chalk from 'chalk'
-import { head, prepend, tail } from 'ramda'
-
 import { apps } from '../../clients'
 import { getAccount, getWorkspace } from '../../conf'
 import { UserCancelledError } from '../../errors'
+import { ManifestEditor, ManifestValidator } from '../../lib/manifest'
 import log from '../../logger'
-import { getManifest, validateApp } from '../../manifest'
 import { promptConfirm } from '../prompts'
-import { toAppLocator } from './../../locator'
 import { parseArgs, validateAppAction } from './utils'
 
 const { uninstallApp } = apps
@@ -24,25 +21,23 @@ const promptAppUninstall = (appsList: string[]): Promise<void> =>
   })
 
 const uninstallApps = async (appsList: string[]): Promise<void> => {
-  if (appsList.length === 0) {
-    return
+  for (const app of appsList) {
+    const appName = ManifestValidator.validateApp(app.split('@')[0], true)
+    try {
+      log.debug('Starting to uninstall app', appName)
+      await uninstallApp(appName)
+      log.info(`Uninstalled app ${appName} successfully`)
+    } catch (e) {
+      log.warn(`The following app was not uninstalled: ${appName}`)
+      log.error(`${e.response.status}: ${e.response.statusText}. ${e.response.data.message}`)
+    }
   }
-  const app = validateApp(head(appsList).split('@')[0], true)
-  try {
-    log.debug('Starting to uninstall app', app)
-    await uninstallApp(app)
-    log.info(`Uninstalled app ${app} successfully`)
-  } catch (e) {
-    log.warn(`The following app was not uninstalled: ${app}`)
-    log.error(`${e.response.status}: ${e.response.statusText}. ${e.response.data.message}`)
-  }
-  await uninstallApps(tail(appsList))
 }
 
 export default async (optionalApp: string, options) => {
   await validateAppAction('uninstall', optionalApp)
-  const app = optionalApp || toAppLocator(await getManifest())
-  const appsList = prepend(app, parseArgs(options._))
+  const app = optionalApp || (await ManifestEditor.getManifestEditor()).appLocator
+  const appsList = [app, ...parseArgs(options._)]
   const preConfirm = options.y || options.yes
 
   if (!preConfirm) {
