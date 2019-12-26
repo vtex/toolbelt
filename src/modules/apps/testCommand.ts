0520adac819f7ef460f2e910f02b458d278edb83
tiagonapoli <tiago.napoli@vtex.com.br>
2019-12-26 10:46:45 -0300

    Change jest test files regex. Change test.ts name

--- src/modules/apps/testCommand.ts
+++ src/modules/apps/testCommand.ts
@@ -1,5 +1,4 @@
 import * as retry from 'async-retry'
-import * as bluebird from 'bluebird'
 import chalk from 'chalk'
 import { concat, map, prop, toPairs } from 'ramda'
 import { createClients } from '../../clients'
@@ -9,7 +8,7 @@ import { toAppLocator } from '../../locator'
 import log from '../../logger'
 import { getAppRoot, getManifest, writeManifestSchema } from '../../manifest'
 import { listenBuild } from '../build'
-import { fixPinnedDependencies, runYarnIfPathExists } from '../utils'
+import { fixPinnedDependencies, PinnedDeps, runYarnIfPathExists } from '../utils'
 import { createLinkConfig, getLinkedFiles, listLocalFiles } from './file'
 import { ProjectUploader } from './ProjectUploader'
 import { pathToFileObject, validateAppAction } from './utils'
@@ -102,9 +101,8 @@ export default async options => {
   const projectUploader = ProjectUploader.getProjectUploader(appId, builder)
 
   try {
-    const aux = await builder.getPinnedDependencies()
-    const pinnedDeps: Map<string, string> = new Map(Object.entries(aux))
-    await bluebird.map(buildersToRunLocalYarn, fixPinnedDependencies(pinnedDeps), { concurrency: 1 })
+    const pinnedDeps: PinnedDeps = await builder.getPinnedDependencies()
+    fixPinnedDependencies(pinnedDeps, manifest.builders)
   } catch (e) {
     log.info('Failed to check for pinned dependencies')
   }
