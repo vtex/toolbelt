16d0232f1acb3924106e971523a0c53eb81cdbda
Alexandre Nicastro <kryndex@gmail.com>
2019-12-27 15:59:28 -0300

    updateNotifier: minor improvements & fixes
    * Fixed message not centered
    * Changed `const` as to be inside the exported function
    * Changed the url of changelog to point to `CHANGELOG.md` file from master instead of the github release/tag

--- src/update.ts
+++ src/update.ts
@@ -3,13 +3,12 @@ import * as updateNotifier from 'update-notifier'
 
 import * as pkg from '../package.json'
 
-const notifier = updateNotifier({ pkg, updateCheckInterval: 1000 * 60 * 60 * 1 })
-
 export default function notify() {
+  const notifier = updateNotifier({ pkg, updateCheckInterval: 1000 * 60 * 60 * 1 })
   if (notifier.update && notifier.update.latest !== pkg.version) {
     const oldVersion = notifier.update.current
     const latestVersion = notifier.update.latest
-    const changelog = `https://github.com/vtex/toolbelt/releases/tag/v${latestVersion}`
+    const changelog = `https://github.com/vtex/toolbelt/blob/master/CHANGELOG.md`
     let type = notifier.update.type
 
     switch (type) {
@@ -27,9 +26,11 @@ export default function notify() {
     notifier.notify({
       isGlobal: true,
       isYarnGlobal: true,
-      message: `New ${type} version of ${pkg.name} available! ${chalk.dim(oldVersion)} → ${chalk.green(latestVersion)}
-        ${chalk.yellow('Changelog:')} ${chalk.cyan(changelog)}
-        Run ${chalk.green(`yarn global add ${pkg.name}`)} to update!`,
+      message: [
+        `New ${type} version of ${pkg.name} available! ${chalk.dim(oldVersion)} → ${chalk.green(latestVersion)}`,
+        `${chalk.yellow('Changelog:')} ${chalk.cyan(changelog)}`,
+        `Run ${chalk.green(`yarn global add ${pkg.name}`)} to update!`,
+      ].join('\n'),
     })
   }
 }
