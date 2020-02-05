25563c0a6352e63c42ddcd6e39337d3d20fbf3aa
tiagonapoli <tiago.napoli@vtex.com.br>
2020-02-04 21:36:01 -0300

    Remove bluebird from 'vtex settings' commands

--- src/modules/apps/settings/index.ts
+++ src/modules/apps/settings/index.ts
@@ -2,14 +2,13 @@ import { path } from 'ramda'
 
 import { apps } from '../../../clients'
 
-const getAppSettings = Promise.method(apps.getAppSettings)
-
 const FIELDS_START_INDEX = 2
 
-export default (app: string, _, options) => {
+export default async (app: string, _, options) => {
   const fields = options._.slice(FIELDS_START_INDEX)
-  return getAppSettings(app)
+  const settingsValues = await apps
+    .getAppSettings(app)
     .then(settings => (fields === null ? settings : path(fields, settings)))
     .then(value => JSON.stringify(value, null, 2))
-    .tap(console.log)
+  console.log(settingsValues)
 }
--- src/modules/apps/settings/unset.ts
+++ src/modules/apps/settings/unset.ts
@@ -2,16 +2,15 @@ import { dissocPath } from 'ramda'
 
 import { apps } from '../../../clients'
 
-const getAppSettings = Promise.method(apps.getAppSettings)
-const saveAppSettings = Promise.method(apps.saveAppSettings)
+const FIELDS_START_INDEX = 1
 
-const FIELDS_START_INDEX = 3
-
-export default (app: string, _, options) => {
+export default async (app: string, _, options) => {
   const fields = options._.slice(FIELDS_START_INDEX)
-  return getAppSettings(app)
+  const newSettingsJson = await apps
+    .getAppSettings(app)
     .then(dissocPath(fields))
     .then(newSettings => JSON.stringify(newSettings, null, 2))
-    .tap(newSettings => saveAppSettings(app, newSettings))
-    .tap(console.log)
+
+  await apps.saveAppSettings(app, newSettingsJson)
+  console.log(newSettingsJson)
 }
