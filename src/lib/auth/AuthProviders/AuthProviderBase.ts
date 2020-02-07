16ef7cba789e539d412919c32a2d8bbe6486f1e5
tiagonapoli <tiago.napoli@vtex.com.br>
2020-02-07 15:17:17 -0300

    Create AuthProviders

---
+++ src/lib/auth/AuthProviders/AuthProviderBase.ts
@@ -0,0 +1,8 @@
+export interface LoginResult {
+  login: string
+  token: string
+}
+
+export abstract class AuthProviderBase {
+  public abstract async login(account: string, workspace: string): Promise<LoginResult>
+}
