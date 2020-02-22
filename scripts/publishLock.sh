f235e122e1cce3aa1a4d2a02b1ec8438b81f74c0
tiagonapoli <tiago.napoli@vtex.com.br>
2020-02-22 14:01:43 -0300

    Add npm publish lock

---
+++ scripts/publishLock.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+BOLD="\e[1m"
+YELLOW="\e[33m"
+
+if [ ! "$IS_CI" == "true" ]; then
+    echo -e "${BOLD}${YELLOW}"
+    echo -e "============================================================================================="
+    echo -e "The CI is configured to publish when a new tag is created."
+    echo -e "If you still want to publish yourself you have to set the env variable IS_CI to \"true\""
+    echo -e "============================================================================================="
+    exit 1
+fi
