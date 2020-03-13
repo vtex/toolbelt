85a43439f31054c55414600d6c86572406216df8
Victor Raposo <vwraposo@gmail.com>
2020-03-13 14:08:00 -0300

    Adds metaInfo logic to continue from where stoped in the file

--- src/modules/rewriter/export.ts
+++ src/modules/rewriter/export.ts
@@ -1,8 +1,9 @@
+import { createHash } from 'crypto'
 import { writeFile, readJson } from 'fs-extra'
 import ora from 'ora'
 
 import { Parser } from 'json2csv'
-import { compose, concat, length, map, range, pluck, prop, sum } from 'ramda'
+import { concat } from 'ramda'
 import { createInterface } from 'readline'
 
 import { rewriter } from '../../clients'
@@ -11,9 +12,8 @@ import { isVerbose } from '../../utils'
 import {
   accountAndWorkspace,
   deleteMetainfo,
-  MAX_ENTRIES_PER_REQUEST,
   MAX_RETRIES,
-  progressBar,
+  METAINFO_FILE,
   saveMetainfo,
   sleep,
   RETRY_INTERVAL_S,
@@ -21,24 +21,29 @@ import {
 } from './utils'
 
 const EXPORTS = 'exports'
+
 const [account, workspace] = accountAndWorkspace
 
 const COLORS = ['cyan', 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray']
 const FIELDS = ['from', 'to', 'type', 'endDate']
 
 const handleExport = async (csvPath: string) => {
-  const spinner = ora('Exporting redirects....').start();
+  const indexHash = await createHash('md5')
+    .update(`${account}_${workspace}_${csvPath}`)
+    .digest('hex')
+  const metainfo = await readJson(METAINFO_FILE).catch(() => ({}))
+  const exportMetainfo = metainfo[EXPORTS] || {}
 
-  // TODO: Add fallback in case there are problems
-  // TODO: CHanges spinner colour according to the number of routes exported
-  // const listener = createInterface({ input: process.stdin, output: process.stdout }).on('SIGINT', () => {
-  //   saveMetainfo(metainfo, EXPORTS, indexHash, counter, listOfRoutes)
-  //   console.log('\n')
-  //   process.exit()
-  // })
+  const spinner = ora('Exporting redirects....').start();
+  // Just need to save the 'next' that had a problem and the listOfRoutes
+  const listener = createInterface({ input: process.stdin, output: process.stdout }).on('SIGINT', () => {
+    saveMetainfo(metainfo, EXPORTS, indexHash, 0, next)
+    console.log('\n')
+    process.exit()
+  })
 
   let listOfRoutes = [] // exportMetainfo[indexHash] ? exportMetainfo[indexHash].data : []
-  let next: string
+  let next: string = exportMetainfo.data
   let count = 2
   do {
     try {
@@ -50,8 +55,8 @@ const handleExport = async (csvPath: string) => {
       next = result.next
       count++
     } catch (e) {
-      // saveMetainfo(metainfo, EXPORTS, indexHash, counter, listOfRoutes)
-      // listener.close()
+      saveMetainfo(metainfo, EXPORTS, indexHash, 0, next)
+      listener.close()
       throw e
     }
   } while (next)
@@ -61,8 +66,8 @@ const handleExport = async (csvPath: string) => {
   const csv = json2csvParser.parse(listOfRoutes)
   await writeFile(`./${csvPath}`, csv)
   log.info('Finished!\n')
-  // listener.close()
-  // deleteMetainfo(metainfo, EXPORTS, indexHash)
+  listener.close()
+  deleteMetainfo(metainfo, EXPORTS, indexHash)
 }
 
 let retryCount = 0
