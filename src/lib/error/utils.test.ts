76f80154e259f135a031d3d9fc4145e926c59b8e
NataliaGodot <natalia_godot@hotmail.com>
2020-03-23 18:10:19 -0300

    Run format-lint

--- src/lib/error/utils.test.ts
+++ src/lib/error/utils.test.ts
@@ -1,12 +1,17 @@
 import { truncateStringsFromObject } from './utils'
 
 it.each([
-    [['123456', '12345', '1234'], 5], 
-    [{ a: '123456', b: '12345', c: ['123456', { a: '123456' }] }, 5],
-    [undefined, 10],
-    [null, 10],
-    [{a: undefined, b: null}, 10],
-    [{a: [{aa: '123456'}, {ab: '1234'}, {ac: '12345'}], b: {ba: undefined, bb: null, bc: '123456', bd: {'bca': '123456', 'bcb': '1234567'}}}]
+  [['123456', '12345', '1234'], 5],
+  [{ a: '123456', b: '12345', c: ['123456', { a: '123456' }] }, 5],
+  [undefined, 10],
+  [null, 10],
+  [{ a: undefined, b: null }, 10],
+  [
+    {
+      a: [{ aa: '123456' }, { ab: '1234' }, { ac: '12345' }],
+      b: { ba: undefined, bb: null, bc: '123456', bd: { bca: '123456', bcb: '1234567' } },
+    },
+  ],
 ])('Works on testcase %# - ', (obj: any, limit: number) => {
   expect(truncateStringsFromObject(obj, limit)).toMatchSnapshot()
 })
