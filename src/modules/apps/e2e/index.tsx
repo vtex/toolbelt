8ab3794b41657287bd4fd4a8f8de484c6e1e1415
TRibeiral <thiagoribeiral98@gmail.com>
2020-03-23 20:25:18 -0300

    refactor e2e index.tsx

--- src/modules/apps/e2e/index.tsx
+++ src/modules/apps/e2e/index.tsx
@@ -6,50 +6,75 @@ import { RealTimeReport } from './report'
 import { getToken } from '../../../conf'
 import { ManifestEditor } from '../../../lib/manifest/ManifestEditor'
 import { ErrorBoundary } from './errorBoundary'
+import { TestRequest } from '../../../clients/Tester'
 
-const TEST_WORKSPACE = ''
-
-const resolveAppLocator = async options => {
-  if (options.workspace) return TEST_WORKSPACE
-  return (await ManifestEditor.getManifestEditor()).appLocator
-}
 
 export default async options => {
-  const cleanAppId = await resolveAppLocator(options)
+  new EndToEndCommand(options).run()
+}
 
-  const appId =
-    cleanAppId === TEST_WORKSPACE
-      ? TEST_WORKSPACE
-      : await apps
-          .listApps()
-          .then(({ data: appList }) => appList.map(({ app }) => app).find(app => app.startsWith(cleanAppId)))
+class EndToEndCommand {
+  options : any
 
-  if (appId === undefined) {
-    throw new Error('Workspace was not found!')
+  constructor(options) {
+    this.options = options
+  }
+  public run() {
+    if(this.options.workspace) {
+      return this.runWorkspaceTests()
+    }
+
+    return this.runAppTests()
   }
 
-  const testRequest = options.report
-    ? null
-    : await tester.test(
-        { integration: true, monitoring: true, authToken: options.token ? getToken() : undefined },
-        appId
-      )
+  private async runAppTests() {
+    const manifestEditor = await ManifestEditor.getManifestEditor()
+    const cleanAppId = await manifestEditor.appLocator
 
-  const testId = testRequest ? testRequest.testId : (options.report as string)
-  const requestedAt = testRequest ? testRequest.requestedAt : null
+    const appList = await apps.listApps()
+    const appId = appList.data.map(({ app }) => app).find(app => app.startsWith(cleanAppId))
+  
+    if (appId === undefined) {
+      throw new Error('App was not found!')
+    }
+  
+    const testRequest = this.options.report
+      ? null
+      : await tester.test(
+          { integration: true, monitoring: true, authToken: this.options.token ? getToken() : undefined },
+          appId
+        )
+  
+    this.render(testRequest)
+  }
 
-  const initialReport = await tester.report(testId)
+  private async runWorkspaceTests() {
+    const testRequest = this.options.report
+      ? null
+      : await tester.test(
+          { integration: true, monitoring: true, authToken: this.options.token ? getToken() : undefined }
+        )
+    
+    this.render(testRequest)
+  }
 
-  render(
-    <ErrorBoundary>
-      <RealTimeReport
-        initialReport={initialReport}
-        testId={testId}
-        poll={() => tester.report(testId)}
-        interval={2000}
-        requestedAt={requestedAt}
-      />
-    </ErrorBoundary>,
-    process.stdout
-  )
+  private async render( testRequest: TestRequest ) {
+    const testId = testRequest ? testRequest.testId : (this.options.report as string)
+    const requestedAt = testRequest ? testRequest.requestedAt : null
+  
+    const initialReport = await tester.report(testId)
+  
+    render(
+      <ErrorBoundary>
+        <RealTimeReport
+          initialReport={initialReport}
+          testId={testId}
+          poll={() => tester.report(testId)}
+          interval={2000}
+          requestedAt={requestedAt}
+        />
+      </ErrorBoundary>,
+      process.stdout
+    )
+  }
 }
