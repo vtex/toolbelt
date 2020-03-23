b4648cc7f8afe7053c317e354ab952615098612b
Rafael Bacelar <rafael.c.bacelar@gmail.com>
2020-03-23 20:04:25 -0300

    Move dotnet builder validation

--- src/modules/debug/dotnet.ts
+++ src/modules/debug/dotnet.ts
@@ -1,7 +1,17 @@
 import { Runtime } from '../../clients/runtime'
 import { getIOContext } from '../utils'
+import { ManifestEditor } from '../../lib/manifest'
+import logger from '../../logger'
 
 export default async (debugInst: string) => {
+  const manifest = await ManifestEditor.getManifestEditor()
+  const { name, vendor, builders } = manifest
+  const { dotnet } = builders
+  if (!dotnet) {
+    logger.error('This command can only be used for dotnet apps')
+    return
+  }
+
   const runtimeClient = new Runtime(getIOContext())
-  await runtimeClient.debugDotnetApp(debugInst)
+  await runtimeClient.debugDotnetApp(name, vendor, manifest.majorRange, debugInst)
 }
