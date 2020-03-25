4abb2936d1a43636a87b8bac54ab25099886ce0f
NataliaGodot <natalia_godot@hotmail.com>
2020-03-25 17:17:35 -0300

    Move utils file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
+import { readdir, stat } from "fs-extra"
+import { resolve } from "path"
+
 export function truncateStringsFromObject(element: any, maxStrSize: number) {
   if (element === null || element === undefined) {
     return element
@@ -12,4 +15,13 @@ export function truncateStringsFromObject(element: any, maxStrSize: number) {
     return `${element.substr(0, maxStrSize)}[...TRUNCATED]`
   }
   return element
+}
+
+export async function readdirRecursive(dir: string) {
+  const subdirs = await readdir(dir)
+  const files = await Promise.all(subdirs.map(async (subdir) => {
+    const res = resolve(dir, subdir)
+    return (await stat(res)).isDirectory() ? readdirRecursive(res) : res;
+  }));
+  return files.reduce((a, f) => a.concat(f), [])
 }
