ab63410927a7ebfcb70e602d2d9a5dbba74fe7dc
TRibeiral <thiagoribeiral98@gmail.com>
2020-03-31 14:55:48 -0300

    refactoring completedTest at test e2e

--- src/modules/apps/e2e/report/completedTest.tsx
+++ src/modules/apps/e2e/report/completedTest.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react'
 import { Box, Color } from 'ink'
-import { groupBy, toPairs } from 'ramda'
 
 import { SpecReport, AppReport } from '../../../../clients/Tester'
 import { FailedSpec } from './failedTest'
 import { AppId } from './app'
 
 
-const completedAppColors = (fail: [string, SpecReport][]) => {
-  if (fail.length === 0) return { bgGreen: true, black: true }
+const completedAppColors = (failedSpecs: SpecResult[]) => {
+  if (failedSpecs.length === 0) return { bgGreen: true, black: true }
   return { bgRed: true, white: true }
 }
 
@@ -17,21 +16,33 @@ export interface AppProps {
   specs: AppReport
 }
 
+interface SpecResult {
+  specName: string
+  specReport: SpecReport
+}
+
 export const Completed: React.FunctionComponent<AppProps> = ({ appId, specs }) => {
-  const specsReports = toPairs(specs)
-  const { fail = [] } = groupBy(([, sr]) => (sr.state === 'passed' ? 'pass' : 'fail'), specsReports)
-  const failed = fail.length > 0
+  const failedSpecs = Object.keys(specs).reduce((acum, curSpecName) => {
+    if(specs[curSpecName].state !== 'passed') {
+      acum.push({ specName: curSpecName, specReport: specs[curSpecName] })
+    }
+
+    return acum
+  }, [] as SpecResult[])
+
+  const failed = failedSpecs.length > 0
+  
   return (
     <Box flexDirection="column">
       <Box>
-        <Color {...completedAppColors(fail)}>{failed ? ' FAIL ' : ' PASS '}</Color>
+        <Color {...completedAppColors(failedSpecs)}>{failed ? ' FAIL ' : ' PASS '}</Color>
         <Box marginLeft={1}>
           <AppId appId={appId} />
         </Box>
       </Box>
       <Box flexDirection="column" marginLeft={2}>
-        {fail.map(([spec, sr]) => (
-          <FailedSpec key={spec} spec={spec} report={sr} />
+        {failedSpecs.map(({ specReport, specName }) => (
+          <FailedSpec key={specName} spec={specName} report={specReport} />
         ))}
       </Box>
     </Box>
