3909a2aeab11bf5e42f364904fc3ae3f81032ea7
TRibeiral <thiagoribeiral98@gmail.com>
2020-03-31 15:41:12 -0300

    e2e report refactoring

--- src/modules/apps/e2e/report/realTime.tsx
+++ src/modules/apps/e2e/report/realTime.tsx
@@ -7,50 +7,50 @@ import { useInterval } from '../useInterval'
 import { Summary } from './summary'
 import { ReportProps, parseReport, Report } from './index'
 
-type RealTimeReport = {
-    testId: string
-    poll: () => Promise<TestReport>
-    interval: number
-    initialReport: TestReport
-    requestedAt?: number
-  }
+interface RealTimeReport {
+  testId: string
+  poll: () => Promise<TestReport>
+  interval: number
+  initialReport: TestReport
+  requestedAt?: number
+}
 
 export const RealTimeReport: React.FunctionComponent<RealTimeReport> = ({
-    testId,
-    poll,
-    interval,
-    initialReport,
-    requestedAt,
-  }) => {
-    const [delay, setDelay] = React.useState(interval)
-    const [report, setReport] = React.useState<ReportProps>(parseReport(initialReport))
-  
-    const handleReport = ({ completed, running }: ReportProps) => {
-      const recentlyCompleted = difference(completed, report.completed)
-  
-      setReport({
-        // careful not to reorder completed apps because they are Static
-        completed: [...report.completed, ...recentlyCompleted],
-        running,
-      })
-  
-      if (running.length === 0) setDelay(null)
-    }
-  
-    useInterval(
-      () =>
-        poll()
-          .then(parseReport)
-          .then(handleReport),
-      delay
-    )
-  
-    return (
-      <Box flexDirection="column">
-        <Report {...report} />
-        <Box marginTop={1}>
-          <Summary {...report} testId={testId} requestedAt={requestedAt} />
-        </Box>
-      </Box>
-    )
+  testId,
+  poll,
+  interval,
+  initialReport,
+  requestedAt,
+}) => {
+  const [delay, setDelay] = React.useState(interval)
+  const [report, setReport] = React.useState<ReportProps>(parseReport(initialReport))
+
+  const handleReport = ({ completedAppTests, runningAppTests }: ReportProps) => {
+    const recentlyCompleted = difference(completedAppTests, report.completedAppTests)
+
+    setReport({
+      // careful not to reorder completed apps because they are Static
+      completedAppTests: [...report.completedAppTests, ...recentlyCompleted],
+      runningAppTests,
+    })
+
+    if (runningAppTests.length === 0) setDelay(null)
   }
+
+  useInterval(
+    () =>
+      poll()
+        .then(parseReport)
+        .then(handleReport),
+    delay
+  )
+
+  return (
+    <Box flexDirection="column">
+      <Report {...report} />
+      <Box marginTop={1}>
+        <Summary {...report} testId={testId} requestedAt={requestedAt} />
+      </Box>
+    </Box>
+  )
+}
