7d4aa994086765d751ab80661b8a4c6589c51f5a
TRibeiral <thiagoribeiral98@gmail.com>
2020-03-31 18:28:16 -0300

    run yarn format-lint

--- src/modules/apps/e2e/report/specsState.ts
+++ src/modules/apps/e2e/report/specsState.ts
@@ -1,7 +1,6 @@
 import { SpecReport, AppReport, TestReport } from '../../../../clients/Tester'
 import { ReportProps } from './index'
 
-
 const COMPLETED_STATES = ['passed', 'failed', 'skipped', 'error']
 
 export const completedSpec = (specReport: SpecReport) => COMPLETED_STATES.includes(specReport.state)
@@ -13,20 +12,20 @@ const completedApp = (appReport: AppReport) => {
 
 export const passedSpec = (specReport: SpecReport) => specReport.state === 'passed'
 export const passedApp = (appReport: AppReport) => {
-    return Object.values(appReport).every((specReport: SpecReport) => {
-      return passedSpec(specReport)
-    })
-  }
+  return Object.values(appReport).every((specReport: SpecReport) => {
+    return passedSpec(specReport)
+  })
+}
 
 export const parseReport = (report: TestReport): ReportProps => {
-    const appTests = Object.keys(report).map((appId) => {
-        return { 
-        appId, 
-        specs: report[appId] 
-        }
-    })
+  const appTests = Object.keys(report).map(appId => {
+    return {
+      appId,
+      specs: report[appId],
+    }
+  })
 
-    const completedAppTests = appTests.filter((appTest) => completedApp(appTest.specs))
-    const runningAppTests = appTests.filter((appTest) => !completedApp(appTest.specs))
-    return { completedAppTests, runningAppTests }
+  const completedAppTests = appTests.filter(appTest => completedApp(appTest.specs))
+  const runningAppTests = appTests.filter(appTest => !completedApp(appTest.specs))
+  return { completedAppTests, runningAppTests }
 }
