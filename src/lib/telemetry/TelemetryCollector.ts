09103edb723ba01e5a4bd316f98906abb4d42890
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-03-31 22:54:28 -0300

    [telemetry:collector] Use PathConstants
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/lib/telemetry/TelemetryCollector.ts
+++ src/lib/telemetry/TelemetryCollector.ts
@@ -1,18 +1,17 @@
+import { spawn } from 'child_process'
 import { randomBytes } from 'crypto'
 import { ensureFileSync, writeJsonSync } from 'fs-extra'
-import { spawn } from 'child_process'
 import { join } from 'path'
-
 import * as pkgJson from '../../../package.json'
-import { ErrorReport, ErrorCreationArguments } from '../error/ErrorReport'
-import { ITelemetryLocalStore, TelemetryLocalStore } from './TelemetryStore'
-import { configDir } from '../../conf'
 import logger from '../../logger'
+import { ErrorCreationArguments, ErrorReport } from '../error/ErrorReport'
 import { Metric, MetricReport } from '../metrics/MetricReport'
+import { PathConstants } from '../PathConstants'
+import { ITelemetryLocalStore, TelemetryLocalStore } from './TelemetryStore'
 
 export class TelemetryCollector {
   private static readonly REMOTE_FLUSH_INTERVAL = 1000 * 60 * 10 // Ten minutes
-  public static readonly TELEMETRY_LOCAL_DIR = join(configDir, 'vtex', 'telemetry')
+  public static readonly TELEMETRY_LOCAL_DIR = PathConstants.TELEMETRY_FOLDER
   private static telemetryCollectorSingleton: TelemetryCollector
 
   public static getCollector() {
