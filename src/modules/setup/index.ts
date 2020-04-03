b6b61af8c99ea6e7857849b800f0f28bc6070e38
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-04-03 16:44:00 -0300

    [cmd:setup] Use new flags
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/modules/setup/index.ts
+++ src/modules/setup/index.ts
@@ -1,14 +1,42 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import chalk from 'chalk'
+import logger from '../../logger'
 import { getManifest } from '../../manifest'
 import { setupTooling } from './setupTooling'
 import { setupTSConfig } from './setupTSConfig'
 import { setupTypings } from './setupTypings'
 
-export default async (opts: { i?: boolean; 'ignore-linked': boolean }) => {
-  const ignoreLinked = opts.i || opts['ignore-linked']
-  const manifest = await getManifest()
+interface SetupOpts {
+  i?: boolean
+  'ignore-linked'?: boolean
+  all?: boolean
+  tooling?: boolean
+  typings?: boolean
+  tsconfig?: boolean
+}
 
-  setupTooling(manifest)
-  await setupTSConfig(manifest)
-  await setupTypings(manifest, ignoreLinked)
+export default async (opts: SetupOpts) => {
+  const all = opts.all || (!opts.tooling && !opts.typings && !opts.tsconfig)
+  const tooling = opts.tooling || opts.all
+  const typings = opts.typings || opts.all
+  const tsconfig = opts.tsconfig || opts.all
+  const ignoreLinked = opts.i || opts['ignore-linked']
+
+  if (ignoreLinked && !(all || typings)) {
+    logger.error(
+      chalk`The flag {bold --ignore-linked (-i)} can only be used when {bold --typings} or {bold --all} are also used`
+    )
+  }
+
+  const manifest = await getManifest()
+  if (tooling) {
+    setupTooling(manifest)
+  }
+
+  if (tsconfig) {
+    await setupTSConfig(manifest, opts.tsconfig)
+  }
+
+  if (typings) {
+    await setupTypings(manifest, ignoreLinked)
+  }
 }
