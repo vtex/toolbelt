87e072be593036025c4e7b85d098516b6ca66844
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-04-06 21:36:29 -0300

    [ErrorKinds] Create invalidOrExpiredTokenError
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/lib/error/ErrorKinds.ts
+++ src/lib/error/ErrorKinds.ts
@@ -1,6 +1,7 @@
 export const enum ErrorKinds {
   REQUEST_ERROR = 'RequestError',
   GENERIC_ERROR = 'GenericError',
+  INVALID_OR_EXPIRED_TOKEN_ERROR = 'InvalidOrExpiredTokenError',
   TELEMETRY_REPORTER_ERROR = 'TelemetryReporterError',
   DEPRECATION_CHECK_ERROR = 'DeprecationCheckError',
   SETUP_TOOLING_ERROR = 'SetupToolingError',
