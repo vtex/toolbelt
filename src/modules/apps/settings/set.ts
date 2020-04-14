6b743f8d74df1edbee29428c7974acb35c371c4b
NataliaGodot <natalia_godot@hotmail.com>
2020-04-14 20:43:27 -0300

    Fix and simplify settings set

--- src/modules/apps/settings/set.ts
+++ src/modules/apps/settings/set.ts
@@ -1,37 +1,12 @@
-import { __, merge, zipObj } from 'ramda'
+import { __, merge } from 'ramda'
 import { apps } from '../../../clients'
-import { parseArgs } from '../utils'
 
-const castValue = value => {
-  let parsedValue
-  try {
-    parsedValue = JSON.parse(value)
-  } catch (err) {
-    parsedValue = value
-  }
-  const numberCast = Number(value)
-  return Number.isNaN(numberCast) ? parsedValue : numberCast
-}
-
-const transformCommandsToObj = commandSettings => {
-  const k = []
-  const v = []
-  for (const [idx, val] of commandSettings.entries()) {
-    const realValue = castValue(val)
-    if (idx % 2) {
-      v.push(realValue)
-    } else {
-      k.push(realValue)
-    }
-  }
-  return zipObj(k, v)
-}
-
-export default async (app: string, _, ___, options) => {
-  const commandSettings = transformCommandsToObj(parseArgs(options._))
+export default async (app: string, field, value) => {
+  const newSetting = {}
+  newSetting[field] = value
   const newSettingsJson = await apps
     .getAppSettings(app)
-    .then(merge(__, commandSettings))
+    .then(merge(__, newSetting))
     .then(newSettings => JSON.stringify(newSettings, null, 2))
 
   await apps.saveAppSettings(app, newSettingsJson)
