83dfb4549462d8e38807bb75f411af52ea69cd42
NataliaGodot <natalia_godot@hotmail.com>
2020-04-15 00:24:27 -0300

    Improve deps update implementation interface and allow to update multiple apps

--- src/commands/deps/update.ts
+++ src/commands/deps/update.ts
@@ -12,16 +12,15 @@ export default class DepsUpdate extends CustomCommand {
     help: oclifFlags.help({ char: 'h' }),
   }
 
-  static args = [{ name: 'appId', required: false }]
+  static args = [{ name: 'appId', required: false }, { name: 'ithAppId', required: false, multiple: true }]
 
   async run() {
     const {
       raw,
-      args: { appId },
     } = this.parse(DepsUpdate)
 
     const allArgs = this.getAllArgs(raw)
 
-    await workspaceDepsUpdate(appId, { _: allArgs })
+    await workspaceDepsUpdate(allArgs)
   }
 }
