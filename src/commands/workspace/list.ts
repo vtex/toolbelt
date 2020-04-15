7643255930072a4b0714dde7b63b09d1ca4adb2b
NataliaGodot <natalia_godot@hotmail.com>
2020-04-15 20:03:48 -0300

    Add workspace ls alias to workspace list

--- src/commands/workspace/list.ts
+++ src/commands/workspace/list.ts
@@ -6,6 +6,8 @@ import workspaceList from '../../modules/workspace/list'
 export default class WorkspaceList extends CustomCommand {
   static description = 'List workspaces on this account'
 
+  static aliases = ['workspace ls']
+
   static examples = ['vtex workspace list', 'vtex workspace ls']
 
   static flags = {
