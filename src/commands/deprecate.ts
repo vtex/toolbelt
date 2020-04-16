0ebe2e76902876accf6f1c8110c23219bcfbda9b
NataliaGodot <natalia_godot@hotmail.com>
2020-04-16 16:59:22 -0300

    Fix multiple argument in deprecate command

--- src/commands/deprecate.ts
+++ src/commands/deprecate.ts
@@ -13,9 +13,11 @@ export default class Deprecate extends CustomCommand {
     yes: oclifFlags.boolean({ description: 'Confirm all prompts', char: 'y', default: false }),
   }
 
+  static strict = false
+
   static args = [
     { name: 'appId', required: false },
-    { name: 'ithAppId', required: false, multipple: true },
+    { name: 'ithAppId', required: false, multiple: true },
   ]
 
   async run() {
