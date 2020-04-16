e8b7636a0925f5a5cdceebc92325f58a0dfa37a7
NataliaGodot <natalia_godot@hotmail.com>
2020-04-16 16:41:15 -0300

    Fix workspace list alias

--- src/commands/workspace/list.ts
+++ src/commands/workspace/list.ts
@@ -6,7 +6,7 @@ import workspaceList from '../../modules/workspace/list'
 export default class WorkspaceList extends CustomCommand {
   static description = 'List workspaces on this account'
 
-  static aliases = ['workspace ls']
+  static aliases = ['workspace:ls']
 
   static examples = ['vtex workspace list', 'vtex workspace ls']
 
