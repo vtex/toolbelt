09162b488986ba35ba66e4f0c05b02334f2beff1
NataliaGodot <natalia_godot@hotmail.com>
2020-04-24 16:13:31 -0300

    Remove example from vtex switch

--- src/commands/switch.ts
+++ src/commands/switch.ts
@@ -6,7 +6,7 @@ import authSwitch from '../modules/auth/switch'
 export default class Switch extends CustomCommand {
   static description = 'Switch to another VTEX account'
 
-  static examples = ['vtex switch storecomponents', 'vtex switch storecomponents/myworkspace']
+  static examples = ['vtex switch storecomponents']
 
   static flags = {
     ...CustomCommand.globalFlags,
