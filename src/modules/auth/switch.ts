79e2552653b1250b6bc8f120e44d4771b6e6ef13
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-04-30 12:15:39 -0300

    [modules:switch] Refactor
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/modules/auth/switch.ts
+++ src/modules/auth/switch.ts
@@ -1,51 +1,93 @@
 import chalk from 'chalk'
 import { split } from 'ramda'
-import { getAccount, getLastUsedAccount, getLogin, getWorkspace } from '../../conf'
 import { CommandError } from '../../errors'
 import { SessionManager } from '../../lib/session/SessionManager'
 import log from '../../logger'
+import { promptConfirm } from '../prompts'
 
-export const switchAccount = async (account: string, options, previousAccount = getAccount()) => {
-  const isValidAccount = /^\s*[\w-]+\s*$/.test(account)
-  const workspace = options.w || options.workspace || 'master'
+interface SwitchOptions {
+  workspace?: string
+  gracefulAccountCheck?: boolean
+  initialPrompt?: {
+    message: string
+  }
+}
+
+interface AccountReturnArgs {
+  previousAccount: string
+  previousWorkspace: string
+  promptConfirmation?: boolean
+}
+
+const checkAndSwitch = async (targetAccount: string, targetWorkspace: string) => {
+  const session = SessionManager.getSingleton()
+  const { account: currAccount } = session
+  const isValidAccount = /^\s*[\w-]+\s*$/.test(targetAccount)
 
   if (!isValidAccount) {
     throw new CommandError('Invalid account format')
-  } else if (!previousAccount) {
+  } else if (!currAccount) {
     throw new CommandError("You're not logged in right now")
-  } else if (previousAccount === account) {
-    throw new CommandError(`You're already using the account ${chalk.blue(account)}`)
+  } else if (currAccount === targetAccount) {
+    throw new CommandError(`You're already using the account ${chalk.blue(targetAccount)}`)
   }
 
-  const sessionManager = SessionManager.getSessionManager()
-  await sessionManager.login(account, { targetWorkspace: workspace })
+  await session.login(targetAccount, { targetWorkspace })
+  const { account, workspace, userLogged } = session
 
-  log.info(
-    `Logged into ${chalk.blue(getAccount())} as ${chalk.green(getLogin())} at workspace ${chalk.green(getWorkspace())}`
-  )
+  log.info(`Logged into ${chalk.blue(account)} as ${chalk.green(userLogged)} at workspace ${chalk.green(workspace)}`)
 }
 
-const hasAccountSwitched = (account: string) => {
-  return account === getAccount()
-}
+export const switchAccount = async (account: string, options: SwitchOptions): Promise<boolean> => {
+  const { account: currAccount, lastUsedAccount } = SessionManager.getSingleton()
 
-export default async (account: string, options) => {
+  if (options.gracefulAccountCheck && currAccount === account) {
+    return false
+  }
+
+  if (options.initialPrompt) {
+    const confirm = await promptConfirm(options.initialPrompt.message)
+    if (!confirm) {
+      return false
+    }
+  }
+
   if (account === '-') {
-    account = getLastUsedAccount()
+    account = lastUsedAccount
     if (account == null) {
       throw new CommandError('No last used account was found')
     }
   }
 
-  const previousAccount = getAccount()
+  const previousAccount = currAccount
   // Enable users to type `vtex switch {account}/{workspace}` and switch
   // directly to a workspace without typing the `-w` option.
   const [parsedAccount, parsedWorkspace] = split('/', account)
   if (parsedWorkspace) {
-    options = { ...options, w: parsedWorkspace, workspace: parsedWorkspace }
+    options = { ...options, workspace: parsedWorkspace }
   }
-  await switchAccount(parsedAccount, options)
-  if (hasAccountSwitched(parsedAccount)) {
-    log.info(`Switched from ${chalk.blue(previousAccount)} to ${chalk.blue(parsedAccount)}`)
-  }
+
+  await checkAndSwitch(parsedAccount, options.workspace || 'master')
+  log.info(`Switched from ${chalk.blue(previousAccount)} to ${chalk.blue(parsedAccount)}`)
+  return true
+}
+
+export function returnToPreviousAccount({
+  previousAccount,
+  previousWorkspace,
+  promptConfirmation = true,
+}: AccountReturnArgs) {
+  return switchAccount(previousAccount, {
+    workspace: previousWorkspace,
+    gracefulAccountCheck: true,
+    ...(promptConfirmation
+      ? {
+          initialPrompt: {
+            message: `Now you are logged in ${chalk.blue(
+              SessionManager.getSingleton().account
+            )}. Do you want to return to ${chalk.blue(previousAccount)} account?`,
+          },
+        }
+      : null),
+  })
 }
