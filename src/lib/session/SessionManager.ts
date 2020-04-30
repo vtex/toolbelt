7609000d64dcef44caa6083cd5227eda7c66769c
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-04-30 12:08:46 -0300

    [SessionManager] Refactor and fix
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/lib/session/SessionManager.ts
+++ src/lib/session/SessionManager.ts
@@ -1,9 +1,9 @@
+import logger from '../../logger'
 import { AuthProviderBase, AuthProviders } from '../auth/AuthProviders'
 import { Token } from '../auth/Token'
-import { SessionsPersister, SessionsPersisterBase } from './SessionsPersister'
-import { ErrorReport } from '../error/ErrorReport'
 import { ErrorKinds } from '../error/ErrorKinds'
-import logger from '../../logger'
+import { ErrorReport } from '../error/ErrorReport'
+import { SessionsPersister, SessionsPersisterBase } from './SessionsPersister'
 
 interface SessionManagerArguments {
   sessionsPersister: SessionsPersisterBase
@@ -16,23 +16,29 @@ interface LoginOptions {
   useCachedToken?: boolean
 }
 
-export class SessionManager {
-  private static sessionManagerSingleton: SessionManager = null
+interface SessionState {
+  account: string
+  tokenObj: Token
+  workspace: string
+  lastAccount: string
+  lastWorkspace: string
+}
 
-  public static getSessionManager(): SessionManager {
-    if (SessionManager.sessionManagerSingleton) {
-      return SessionManager.sessionManagerSingleton
+export class SessionManager {
+  private static singleton: SessionManager
+
+  public static getSingleton(): SessionManager {
+    if (SessionManager.singleton) {
+      return SessionManager.singleton
     }
 
     const sessionsPersister = SessionsPersister.getSingleton()
     const authProviders = AuthProviders.getAuthProviders()
-    SessionManager.sessionManagerSingleton = new SessionManager({ sessionsPersister, authProviders })
-    return SessionManager.sessionManagerSingleton
+    SessionManager.singleton = new SessionManager({ sessionsPersister, authProviders })
+    return SessionManager.singleton
   }
 
-  private currAccount: string
-  private currToken: Token
-  private currWorkspace: string
+  private state: SessionState
 
   private sessionPersister: SessionManagerArguments['sessionsPersister']
   private authProviders: SessionManagerArguments['authProviders']
@@ -40,41 +46,46 @@ export class SessionManager {
   constructor({ sessionsPersister, authProviders }: SessionManagerArguments) {
     this.sessionPersister = sessionsPersister
     this.authProviders = authProviders
-    this.currAccount = this.sessionPersister.getAccount()
-    this.currWorkspace = this.sessionPersister.getWorkspace()
-    this.currToken = new Token(this.sessionPersister.getToken())
+    this.state = {
+      account: this.sessionPersister.getAccount(),
+      lastAccount: this.sessionPersister.getLastAccount(),
+      workspace: this.sessionPersister.getWorkspace(),
+      lastWorkspace: this.sessionPersister.getLastWorkspace(),
+      tokenObj: new Token(this.sessionPersister.getToken()),
+    }
   }
 
   get account() {
-    return this.currAccount
+    return this.state.account
   }
 
   get token() {
-    return this.currToken.token
+    return this.state.tokenObj.token
   }
 
   get tokenObj() {
-    return this.currToken
+    return this.state.tokenObj
   }
 
   get workspace() {
-    return this.currWorkspace
+    return this.state.workspace
   }
 
   get userLogged() {
-    return this.currToken.login
+    return this.state.tokenObj.login
   }
 
-  private saveCredentials() {
-    this.sessionPersister.saveAccount(this.account)
-    this.sessionPersister.saveWorkspace(this.workspace)
-    this.sessionPersister.saveLogin(this.currToken.login)
-    this.sessionPersister.saveToken(this.currToken.token)
+  get lastUsedAccount() {
+    return this.state.lastAccount
+  }
+
+  get lastUsedWorkspace() {
+    return this.state.lastWorkspace
   }
 
   public checkAndGetToken(exitOnInvalid = false) {
-    if (this.currToken.isValid()) {
-      return this.currToken.token
+    if (this.state.tokenObj.isValid()) {
+      return this.state.tokenObj.token
     }
 
     const errMsg = 'Auth token is invalid or expired.'
@@ -94,29 +105,70 @@ export class SessionManager {
     newAccount: string,
     { targetWorkspace = 'master', authMethod = 'oauth', useCachedToken = true }: LoginOptions
   ) {
-    const currentToken = new Token(this.sessionPersister.getAccountToken(newAccount))
-    if (useCachedToken && currentToken.isValid()) {
-      this.currAccount = newAccount
-      this.currWorkspace = targetWorkspace
-      this.currToken = currentToken
-      this.saveCredentials()
+    if (this.account !== newAccount) {
+      this.state.lastAccount = this.account
+      this.state.lastWorkspace = null
+    }
+
+    const cachedToken = new Token(this.sessionPersister.getAccountToken(newAccount))
+    if (useCachedToken && cachedToken.isValid()) {
+      this.state.account = newAccount
+      this.state.workspace = targetWorkspace
+      this.state.tokenObj = cachedToken
+      this.flushState()
       return
     }
 
-    const { token } = await this.authProviders[authMethod].login(newAccount, targetWorkspace)
-    this.currAccount = newAccount
-    this.currWorkspace = targetWorkspace
-    this.currToken = new Token(token)
-    this.saveCredentials()
-    this.sessionPersister.saveAccountToken(newAccount, this.currToken.token)
+    // Tokens are scoped by workspace - logging into master will grant cacheability
+    const { token } = await this.authProviders[authMethod].login(newAccount, 'master')
+    this.state.account = newAccount
+    this.state.workspace = targetWorkspace
+    this.state.tokenObj = new Token(token)
+    this.flushState()
+    this.sessionPersister.saveAccountToken(newAccount, this.state.tokenObj.token)
   }
 
   public logout() {
     this.sessionPersister.clearData()
   }
 
-  public switchWorkspace(newWorkspace: string) {
-    this.currWorkspace = newWorkspace
-    this.sessionPersister.saveWorkspace(this.currWorkspace)
+  public workspaceSwitch(newWorkspace: string) {
+    if (this.state.workspace === newWorkspace) {
+      return
+    }
+
+    this.state.lastWorkspace = this.state.workspace
+    this.state.workspace = newWorkspace
+    this.flushWorkspaceData()
+  }
+
+  /* This should not be used - implement another login method instead */
+  public DEPRECATEDchangeAccount(account: string) {
+    this.state.lastAccount = this.state.account
+    this.state.account = account
+    this.flushState()
+  }
+
+  /* This should not be used - implement another login method instead */
+  public DEPRECATEDchangeToken(token: string) {
+    this.state.tokenObj = new Token(token)
+    this.flushState()
+  }
+
+  private flushState() {
+    this.flushAccountData()
+    this.flushWorkspaceData()
+    this.sessionPersister.saveLogin(this.state.tokenObj.login)
+    this.sessionPersister.saveToken(this.state.tokenObj.token)
+  }
+
+  private flushWorkspaceData() {
+    this.sessionPersister.saveWorkspace(this.state.workspace)
+    this.sessionPersister.saveLastWorkspace(this.state.lastWorkspace)
+  }
+
+  private flushAccountData() {
+    this.sessionPersister.saveAccount(this.state.account)
+    this.sessionPersister.saveLastAccount(this.state.lastAccount)
   }
 }
