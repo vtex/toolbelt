634bfff2e2ea4edcd3bf6e8b30cc37d116106d8b
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-05-07 16:23:28 -0300

    [clients] Migrate NpmClient
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/lib/clients/NpmClient.ts
+++ src/lib/clients/NpmClient.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { PackageJson } from '../lib/packageJson'
+import { PackageJson } from '../packageJson'
 
 interface PackageMetadata extends PackageJson {
   deprecated?: string
