84b851b4ddb8a7ce7135739d3add0431096bb3f5
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-05-07 16:40:20 -0300

    [clients] Migrate Sponsor
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/modules/sponsor/getEdition.ts
+++ src/modules/sponsor/getEdition.ts
@@ -1,12 +1,10 @@
 import chalk from 'chalk'
-import { createIOContext } from '../../lib/clients'
 import { Sponsor } from '../../lib/clients/Sponsor'
 import { SessionManager } from '../../lib/session/SessionManager'
 import log from '../../logger'
-import { IOClientOptions } from '../utils'
 
 export default async () => {
-  const sponsorClient = new Sponsor(createIOContext(), IOClientOptions)
+  const sponsorClient = Sponsor.createClient()
   const data = await sponsorClient.getEdition()
   log.info(
     `Current edition for account ${chalk.blue(SessionManager.getSingleton().account)} is ${chalk.green(data.id)}`
