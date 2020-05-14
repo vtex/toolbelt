937e764781636cab574146b6918d7681c691fc69
Jeymisson Oliveira <jeymissoncz@gmail.com>
2020-05-14 13:12:13 -0300

    Wrap logsEventSource in a class

--- src/commands/logs.ts
+++ src/commands/logs.ts
@@ -1,7 +1,7 @@
 import { flags as oclifFlags } from '@oclif/command'
 
 import { CustomCommand } from '../oclif/CustomCommand'
-import appsLogs from '../modules/apps/logs'
+import appsLogs from '../modules/apps/logs/logs'
 
 export default class Logs extends CustomCommand {
   static description = 'Show logs of an app on the current directory or a specified one'
