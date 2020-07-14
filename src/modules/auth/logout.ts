c7920b8b2c247b6f8b2087057b6ac8179a716557
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-07-14 19:55:23 -0300

    Improve logout
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/modules/auth/logout.ts
+++ src/modules/auth/logout.ts
@@ -4,6 +4,6 @@ import log from '../../api/logger'
 export default () => {
   log.debug('Clearing config file')
   const sessionManager = SessionManager.getSingleton()
-  sessionManager.logout()
+  sessionManager.logout({ invalidateBrowserAuthCookie: true })
   log.info('See you soon!')
 }
