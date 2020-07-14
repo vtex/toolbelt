eebe0352dcf4d346f1f339e67cc94ba63c8f4dc1
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-07-14 20:12:15 -0300

    [clients:vtexid] Add token invalidation functions
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/api/clients/IOClients/external/VTEXID.ts
+++ src/api/clients/IOClients/external/VTEXID.ts
@@ -1,5 +1,7 @@
 import { InstanceOptions, IOClient, IOContext } from '@vtex/api'
+import opn from 'opn'
 import querystring from 'querystring'
+import { storeUrl } from '../../../domain'
 import { IOClientFactory } from '../IOClientFactory'
 
 export class VTEXID extends IOClient {
@@ -7,9 +9,17 @@ export class VTEXID extends IOClient {
   private static readonly BASE_URL = 'https://vtexid.vtex.com.br'
   private static readonly API_PATH_PREFIX = '/api/vtexid'
   private static readonly TOOLBELT_API_PATH_PREFIX = `${VTEXID.API_PATH_PREFIX}/toolbelt`
+  private static readonly VTEX_ID_AUTH_COOKIE = 'VtexIdClientAutCookie'
 
   public static createClient(customContext: Partial<IOContext> = {}, customOptions: Partial<InstanceOptions> = {}) {
     return IOClientFactory.createClient<VTEXID>(VTEXID, customContext, customOptions, { requireAuth: false })
+  }
+
+  public static invalidateBrowserAuthCookie(account: string) {
+    return opn(
+      storeUrl({ account, addWorkspace: false, path: `${VTEXID.API_PATH_PREFIX}/pub/single-sign-out?scope=` }),
+      { wait: false }
+    )
   }
 
   constructor(ioContext: IOContext, opts: InstanceOptions) {
@@ -37,6 +47,14 @@ export class VTEXID extends IOClient {
     })
 
     return this.http.post<{ token: string }>(`${VTEXID.TOOLBELT_API_PATH_PREFIX}/validate?an=${account}`, body)
+  }
+
+  public invalidateToolbeltToken(token: string) {
+    return this.http.get(`/api/vtexid/pub/logout?scope=`, {
+      headers: {
+        Cookie: `${VTEXID.VTEX_ID_AUTH_COOKIE}=${token}`,
+      },
+    })
   }
 }
 
