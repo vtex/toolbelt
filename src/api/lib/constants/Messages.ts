be19ecf1d73528e090826e1520644560f9e42091
Larícia Mota <laricia.mota@vtex.com.br>
2020-08-12 18:27:18 -0300

    Move messages file to api

--- src/api/lib/constants/Messages.ts
+++ src/api/lib/constants/Messages.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk'
-import { formatHyperlink } from '../utils/Messages'
+import { formatHyperlink } from '../../lib/utils/Messages'
 import { ColorifyConstants } from './Colors'
 
 export const Messages = {
@@ -41,4 +41,16 @@ export const Messages = {
     `${chalk.bold('Workspace change:')} You are now using the workspace ${ColorifyConstants.ID(
       workspace
     )} on account ${ColorifyConstants.ID(account)}.\n`,
+  DEPS_DIFF_INIT: (workspace1: string, workspace2: string) =>
+    `${chalk.bold('Dependency diff')} between ${ColorifyConstants.ID(workspace1)} and ${ColorifyConstants.ID(
+      workspace2
+    )}`,
+  DEPS_DIFF_EMPTY: (workspace1: string, workspace2: string) =>
+    `${Messages.DEPS_DIFF_INIT(workspace1, workspace2)} is empty\n`,
+  DEPS_LIST_INIT: 'Starting to list dependencies',
+  DEPS_UPDATE_INIT: (locator: string) => `Starting to update ${locator}`,
+  DEPS_UPDATE_INIT_PROCESS: 'Starting update process',
+  DEPS_UPDATE_INVALID_FORMAT_ERROR: (locator: string) =>
+    `App ${locator} has an invalid app format, please use <vendor>.<name>@<version>`,
+  DEPS_UPDATE_EMPTY: 'No dependencies updated',
 }
