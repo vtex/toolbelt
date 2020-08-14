7fc45ff941c52a5b86eb2e72f3762db4ff4acb78
Thiago Veras <thiago.veras.machado@gmail.com>
2020-08-14 15:52:31 -0300

    lint

--- src/api/modules/workspace/create.ts
+++ src/api/modules/workspace/create.ts
@@ -105,7 +105,9 @@ export const workspaceCreator: WorkspaceCreator = async ({
   } catch (err) {
     if (err.response?.data.code === 'ArgumentNull') {
       throw createFlowIssueError(
-        `You need to pick a name for the new workspace. Run ${chalk.yellow('vtex workspace create [WORKSPACENAME]')}. You can name it using your name with a number, for example vtex workspace create johndoe01`
+        `You need to pick a name for the new workspace. Run ${chalk.yellow(
+          'vtex workspace create [WORKSPACENAME]'
+        )}. You can name it using your name with a number, for example vtex workspace create johndoe01`
       )
     }
 
