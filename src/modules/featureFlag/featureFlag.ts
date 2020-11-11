a396772dfa13513a98d337c51b338770441e203a
Thiago Veras <thiago.veras.machado@gmail.com>
2020-11-11 17:31:02 -0300

    Add featureFlag base files

---
+++ src/modules/featureFlag/featureFlag.ts
@@ -0,0 +1,45 @@
+import Configstore from 'configstore'
+
+export interface IFeatureFlag {
+  storeFilePath: string
+  getLastFeatureFlagUpdate: () => number
+  setLastFeatureFlagUpdate: (date: number) => void
+  getFeatureFlagInfo: () => Record<string, any>
+  setFeatureFlagInfo: (info: Record<string, any>) => void
+}
+
+export class FeatureFlag implements IFeatureFlag {
+  public static readonly FEATURE_FLAG_STORE_FILENAME = 'feature-flag.json'
+  private static singleton: FeatureFlag
+
+  public static getSingleton(storeFilePath?: string): FeatureFlag {
+    if (FeatureFlag.singleton) {
+      return FeatureFlag.singleton
+    }
+
+    FeatureFlag.singleton = new FeatureFlag(storeFilePath)
+    return FeatureFlag.singleton
+  }
+
+  private store: Configstore
+  
+  constructor(public storeFilePath: string) {
+    this.store = new Configstore('', null, { configPath: storeFilePath })
+  }
+
+  getLastFeatureFlagUpdate() {
+    return (this.store.get('lastFeatureFlagCheck') as number) ?? 0
+  }
+
+  getFeatureFlagInfo() {
+    return (this.store.get('featureFlagInfo') as Record<string, any> | null) ?? {}
+  }
+
+  setLastFeatureFlagUpdate(date: number) {
+    this.store.set('lastFeatureFlagCheck', date)
+  }
+
+  setFeatureFlagInfo(versionFeatureFlagInfo: Record<string, any>) {
+    this.store.set('featureFlagInfo', versionFeatureFlagInfo)
+  }
+}
---
+++ src/modules/featureFlag/featureFlagUpdateExec.ts
@@ -0,0 +1,39 @@
+const initTime = process.hrtime()
+
+import { ToolbeltConfig } from '../../api/clients/IOClients/apps/ToolbeltConfig'
+import { ErrorKinds } from '../../api/error/ErrorKinds'
+import { ErrorReport } from '../../api/error/ErrorReport'
+import { TelemetryCollector } from '../../lib/telemetry/TelemetryCollector'
+import { hrTimeToMs } from '../../lib/utils/hrTimeToMs'
+import { IFeatureFlag, FeatureFlag } from './featureFlag';
+
+export const updateFeatureFlagsFile = async (store: IFeatureFlag) => {
+  try {
+    const client = ToolbeltConfig.createClient({}, { retries: 3 })
+    const { featureFlags } = await client.getGlobalConfig()
+
+    store.setFeatureFlagInfo(featureFlags)
+    store.setLastFeatureFlagUpdate(Date.now())
+
+  } catch (err) {
+    const telemetryCollector = TelemetryCollector.getCollector()
+    const errorReport = telemetryCollector.registerError(
+      ErrorReport.create({
+        kind: ErrorKinds.FEATURE_FLAG_CHECK_ERROR,
+        originalError: err,
+      })
+    )
+
+    console.error('Error checking for feature flag', JSON.stringify(errorReport.toObject(), null, 2))
+    telemetryCollector.flush()
+  }
+}
+
+if (require.main === module) {
+  // eslint-disable-next-line prefer-destructuring
+  const storeFilePath = process.argv[2]
+  const store = FeatureFlag.getSingleton(storeFilePath)
+  // eslint-disable-next-line prefer-destructuring
+  updateFeatureFlagsFile(store)
+  console.log(`Finished checking for feature flag after ${hrTimeToMs(process.hrtime(initTime))}`)
+}
