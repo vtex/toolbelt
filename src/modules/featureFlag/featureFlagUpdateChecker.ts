9d47d28aadfb1ae52eb86f9de934e08fc582fa16
Thiago Veras <thiago.veras.machado@gmail.com>
2020-11-11 17:31:02 -0300

    Add featureFlag base files

---
+++ src/modules/featureFlag/featureFlagUpdateChecker.ts
@@ -0,0 +1,25 @@
+import { join } from 'path'
+import { spawnUnblockingChildProcess } from '../../lib/utils/spawnUnblockingChildProcess'
+import { FeatureFlag } from './featureFlag'
+
+export class FeatureFlagUpdateChecker {
+  private static readonly FEATURE_FLAG_CHECK_INTERVAL = 1000 * 60 * 2 // 2 Minutes
+
+  public static checkForUpdateFeatureFlag() {
+    if (FeatureFlagUpdateChecker.shouldUpdateFeatureFlagFile()) {
+      FeatureFlagUpdateChecker.startUpdateFileProcess()
+    }
+  }
+
+  private static shouldUpdateFeatureFlagFile() {
+    return Date.now() - FeatureFlag.getSingleton().getLastFeatureFlagUpdate() >= FeatureFlagUpdateChecker.FEATURE_FLAG_CHECK_INTERVAL
+  }
+
+  private static startUpdateFileProcess() {
+    spawnUnblockingChildProcess(process.execPath, [
+      join(__dirname, 'featureFlagUpdateExec.js'),
+      FeatureFlag.getSingleton().storeFilePath
+    ])
+  }
+
+}
