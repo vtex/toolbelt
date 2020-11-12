62bd4aff52891d1cf29d506243baecbc6bdbdee4
Thiago Veras <thiago.veras.machado@gmail.com>
2020-11-12 18:17:50 -0300

    Refactor feature flag constructor

--- src/modules/featureFlag/featureFlag.ts
+++ src/modules/featureFlag/featureFlag.ts
@@ -1,10 +1,13 @@
 import Configstore from 'configstore'
+import { join } from 'path'
+import { CLIPreTasks } from '../../CLIPreTasks/CLIPreTasks'
 
 export interface IFeatureFlag {
   storeFilePath: string
   getLastFeatureFlagUpdate: () => number
   setLastFeatureFlagUpdate: (date: number) => void
-  getFeatureFlagInfo: () => Record<string, any>
+  getAllFeatureFlagInfo: () => Record<string, any>
+  getFeatureFlagInfo: <T>(flagName: string) => T
   setFeatureFlagInfo: (info: Record<string, any>) => void
 }
 
@@ -12,12 +15,14 @@ export class FeatureFlag implements IFeatureFlag {
   public static readonly FEATURE_FLAG_STORE_FILENAME = 'feature-flag.json'
   private static singleton: FeatureFlag
 
-  public static getSingleton(storeFilePath?: string): FeatureFlag {
+  public static getSingleton(): FeatureFlag {
     if (FeatureFlag.singleton) {
       return FeatureFlag.singleton
     }
 
-    FeatureFlag.singleton = new FeatureFlag(storeFilePath)
+    FeatureFlag.singleton = new FeatureFlag(
+      join(CLIPreTasks.PRETASKS_LOCAL_DIR, FeatureFlag.FEATURE_FLAG_STORE_FILENAME)
+    )
     return FeatureFlag.singleton
   }
 
@@ -31,8 +36,12 @@ export class FeatureFlag implements IFeatureFlag {
     return (this.store.get('lastFeatureFlagCheck') as number) ?? 0
   }
 
-  getFeatureFlagInfo() {
+  getAllFeatureFlagInfo() {
     return (this.store.get('featureFlagInfo') as Record<string, any> | null) ?? {}
+  }
+
+  getFeatureFlagInfo<T>(flagName: string): T {
+    return this.store.get(`featureFlagInfo.${flagName}`) as T
   }
 
   setLastFeatureFlagUpdate(date: number) {
