929dfaca24303024caae54d61e695456e1662b50
Thiago Veras <thiago.veras.machado@gmail.com>
2021-02-19 17:59:59 -0300

    Fix 25ms to 25000ms (#1028)

--- src/api/modules/workspace/create.ts
+++ src/api/modules/workspace/create.ts
@@ -71,7 +71,7 @@ export const workspaceCreator: WorkspaceCreator = async ({
   }
 
   const { account, workspace, token } = clientContext ?? SessionManager.getSingleton()
-  const workspaces = createWorkspacesClient({ workspace, account, authToken: token }, { timeout: 25 })
+  const workspaces = createWorkspacesClient({ workspace, account, authToken: token }, { timeout: 25000 })
 
   if (await workspaceExists(account, targetWorkspace, workspaces)) {
     maybeLogWorkspaceAlreadyExists(targetWorkspace, logIfAlreadyExists)
