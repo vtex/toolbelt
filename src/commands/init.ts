93d5ed8ba078fc1f35a934b9971756ea70cc0369
Gabriel Tibúrcio <tibuurcio@gmail.com>
2021-02-23 17:41:20 -0300

    adds folder name option to init command and checks for the default_branch before downloading it from github

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -4,17 +4,19 @@ import appsInit from '../modules/init'
 export default class Init extends CustomCommand {
   static description = 'Create basic files and folders for your VTEX app'
 
-  static examples = ['vtex init']
+  static examples = ['vtex init', 'vtex init project-name']
 
   static flags = {
     ...CustomCommand.globalFlags,
   }
 
-  static args = []
+  static args = [{ name: 'projectName', required: false }]
 
   async run() {
-    this.parse(Init)
+    const {
+      args: { projectName },
+    } = this.parse(Init)
 
-    await appsInit()
+    await appsInit(projectName)
   }
 }
