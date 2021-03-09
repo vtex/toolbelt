8b1b4f9a8fef6e538cb12b2958fe385ac26db899
Gabriel Tibúrcio <tibuurcio@gmail.com>
2021-03-09 17:29:00 -0300

    Release v3.2.0-beta
    * adds folder name option to init command and checks for the default_branch before downloading it from github
    
    * move messages to Messages.ts file and use ColorifyConstants instead of chalk
    
    * Release v3.2.0-beta
    
    Co-authored-by: Thiago Veras <thiago.veras.machado@gmail.com>

--- src/modules/init/git.ts
+++ src/modules/init/git.ts
@@ -2,16 +2,33 @@ import { copy, emptyDir, ensureDir, remove } from 'fs-extra'
 import pipeStreams from 'pipe-streams-to-promise'
 import request from 'request'
 import unzip from 'unzip-stream'
+import axios from 'axios'
+import log from '../../api/logger'
+import { Messages } from '../../lib/constants/Messages'
 
-const urlForRepo = (repo: string, org: string) => `https://github.com/${org}/${repo}/archive/master.zip`
+const urlForRepo = async (repo: string, org: string) => {
+  /**
+   * If we have internal templates in the future (in a private org repo) we have to figure out
+   * how to make this request authenticated. But, since toolbelt is open-source and so are the
+   * apps templates, we can afford not to take care of this right now.
+   */
+  try {
+    const { default_branch: branchName } = (await axios.get(`https://api.github.com/repos/${org}/${repo}`)).data
+    return { url: `https://github.com/${org}/${repo}/archive/${branchName}.zip`, branchName }
+  } catch (err) {
+    log.debug(err)
+    throw new Error(Messages.ERROR_COULD_NOT_DETERMINE_DEFAULT_BRANCH(org, repo))
+  }
+}
 
 const fetchAndUnzip = async (url: string, path: string) => pipeStreams([request(url), unzip.Extract({ path })])
 
-export const clone = async (repo: string, org: string) => {
+export const clone = async (repo: string, org: string, projectFolderName?: string) => {
   const cwd = process.cwd()
-  const url = urlForRepo(repo, org)
-  const destPath = `${cwd}/${repo}`
-  const cloned = `${destPath}/${repo}-master`
+  const { url, branchName } = await urlForRepo(repo, org)
+  log.debug(Messages.DEBUG_DOWNLOAD_TEMPLATE_URL(url))
+  const destPath = `${cwd}/${projectFolderName ?? repo}`
+  const cloned = `${destPath}/${repo}-${branchName}`
 
   await ensureDir(destPath)
   await emptyDir(destPath)
--- src/modules/init/index.ts
+++ src/modules/init/index.ts
@@ -7,6 +7,7 @@ import { promptConfirm } from '../../api/modules/prompts'
 
 import * as git from './git'
 import { SessionManager } from '../../api/session/SessionManager'
+import { Messages } from '../../lib/constants/Messages'
 
 const VTEX_APPS = 'vtex-apps'
 
@@ -104,9 +105,9 @@ const promptTemplates = async (): Promise<string> => {
   return chosen
 }
 
-const promptContinue = async (repoName: string) => {
+const promptContinue = async (repoName: string, projectFolderName?: string) => {
   const proceed = await promptConfirm(
-    `You are about to create the new folder ${process.cwd()}/${repoName}. Do you want to continue?`
+    Messages.PROMPT_CONFIRM_NEW_FOLDER(`${process.cwd()}/${projectFolderName ?? repoName}`)
   )
   if (!proceed) {
     log.info('Bye o/')
@@ -114,15 +115,15 @@ const promptContinue = async (repoName: string) => {
   }
 }
 
-export default async () => {
-  log.debug('Prompting for app info')
-  log.info('Hello! I will help you generate basic files and folders for your app.')
+export default async (projectFolderName?: string) => {
+  log.debug(Messages.DEBUG_PROMPT_APP_INFO)
+  log.info(Messages.INIT_HELLO_EXPLANATION())
   try {
     const { repository: repo, organization: org } = templates[await promptTemplates()]
-    await promptContinue(repo)
+    await promptContinue(repo, projectFolderName)
     log.info(`Cloning ${chalk.bold.cyan(repo)} from ${chalk.bold.cyan(org)}.`)
-    await git.clone(repo, org)
-    log.info(`Run ${chalk.bold.green(`cd ${repo}`)} and ${chalk.bold.green('vtex link')} to start developing!`)
+    await git.clone(repo, org, projectFolderName)
+    log.info(Messages.INIT_START_DEVELOPING(projectFolderName ?? repo))
   } catch (err) {
     log.error(err.message)
     log.debug(err)
