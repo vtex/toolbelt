5c494546d504047d7c7da5e163fc3af99c42bf52
Victor Elias <victor.elias@vtex.com.br>
2021-04-20 14:06:04 -0300

    Fix edition set command error handling (#1068)
    * Prompt install tenant provisioner on 404 error
    
    * Actually stop the flow when user negates prompts
    
    We were just ignoring their responses, not stopping the flow,
    and continuing the execution as if nothing happened xD
    
    * Fix master workspace prompt
    
    * Create a helper function for the environment switch
    
    * Fix appsInstall function installApps functions
    
    They can't point to some static clients or they won't grab
    any context switch that may have been made. Create indirect
    functions instead which actually create a client and call
    the function forwarding all the arguments, to guarantee they
    get clients in the current context (like after switching acc).
    
    * Implement some retry logic to account for stale cache
    
    * Create helper function to handle all retries
    
    * Only retry on actual route not found errors
    
    Do not retry in case the tenant provisioner itself has returned an error
    
    * Add a comment to explain the retry logic
    
    * Prettier
    
    * Move sleepSec functin to closest utils file
    
    * Changelog
    
    * Lint :annoyed:

--- src/api/modules/sponsor/setEdition.ts
+++ src/api/modules/sponsor/setEdition.ts
@@ -1,9 +1,11 @@
 import chalk from 'chalk'
+
+import appsInstall from '../../../modules/apps/install'
 import { createFlowIssueError } from '../../error/utils'
 import { Sponsor } from '../../clients/IOClients/apps/Sponsor'
 import { SessionManager } from '../../session/SessionManager'
 import log from '../../logger'
-import { promptWorkspaceMaster } from '../utils'
+import { promptWorkspaceMaster, sleepSec } from '../utils'
 import { returnToPreviousAccount, switchAccount } from '../auth/switch'
 import { promptConfirm } from '../prompts'
 
@@ -13,9 +15,90 @@ const promptSwitchToAccount = async (account: string, initial: boolean) => {
     : `Only current account sponsor (${chalk.blue(account)}) can change its edition`
   const proceed = await promptConfirm(`${reason}. Do you want to switch to account ${chalk.blue(account)}?`)
   if (!proceed) {
-    return
+    return false
   }
   await switchAccount(account, {})
+  return true
+}
+
+const confirmAndSwitchEnvironment = async (sponsorAccount: string, targetAccount: string, targetWorkspace: string) => {
+  if (!sponsorAccount) {
+    if (targetWorkspace !== 'master') {
+      throw createFlowIssueError('Can only set initial edition in master workspace')
+    }
+    return promptSwitchToAccount('vtex', true)
+  }
+
+  const workspaceIsOk = targetWorkspace !== 'master' || (await promptWorkspaceMaster(targetAccount))
+  if (!workspaceIsOk) return false
+  return promptSwitchToAccount(sponsorAccount, false)
+}
+
+const tenantProvisionerApp = 'vtex.tenant-provisioner'
+
+const promptInstallTenantProvisioner = async (account: string) => {
+  log.warn(`Tenant provisioner app seems not to be installed in sponsor account ${chalk.blue(account)}.`)
+  const proceed = await promptConfirm(
+    `Do you want to install ${chalk.blue(tenantProvisionerApp)} in account ${chalk.blue(account)} now?`
+  )
+  if (!proceed) {
+    return false
+  }
+  await appsInstall([tenantProvisionerApp], { force: false })
+  return true
+}
+
+const isProvisionerNotInstalledError = (err: any) => {
+  const res = err.response
+  return res?.status === 404 && res.data?.source === 'Vtex.Kube.Router' && res.data?.code === 'NotFound'
+}
+
+const trySetEditionOnce = async (client: Sponsor, targetAccount: string, targetWorkspace: string, edition: string) => {
+  try {
+    await client.setEdition(targetAccount, targetWorkspace, edition)
+    return true
+  } catch (err) {
+    if (!isProvisionerNotInstalledError(err)) {
+      throw err
+    }
+    return false
+  }
+}
+
+const maxSetEditionRetries = 3
+
+const trySetEdition = async (
+  sponsorAccount: string,
+  targetAccount: string,
+  targetWorkspace: string,
+  edition: string
+) => {
+  const client = Sponsor.createClient()
+  let success = await trySetEditionOnce(client, targetAccount, targetWorkspace, edition)
+  if (success) {
+    return true
+  }
+
+  const installed = await promptInstallTenantProvisioner(sponsorAccount)
+  if (!installed) {
+    return false
+  }
+
+  const workspaceNotice = targetWorkspace === 'master' ? '' : `in workspace ${chalk.blue(targetWorkspace)} `
+  log.info(`Now setting edition ${chalk.blue(edition)} ${workspaceNotice}of account ${chalk.blue(targetAccount)}...`)
+
+  // Retry a couple of times since it might take some time for the installation to propagate until the route is available.
+  /* eslint-disable no-await-in-loop */
+  for (let retry = 1; !success && retry <= maxSetEditionRetries; retry++) {
+    await sleepSec(1.5 * retry)
+
+    success =
+      retry < maxSetEditionRetries
+        ? await trySetEditionOnce(client, targetAccount, targetWorkspace, edition)
+        : await client.setEdition(targetAccount, targetWorkspace, edition).then(() => true)
+  }
+  /* eslint-enable no-await-in-loop */
+  return success
 }
 
 export default async function setEdition(edition: string, workspace?: string, autoSwitchBack = false) {
@@ -31,26 +114,19 @@ export default async function setEdition(edition: string, workspace?: string, au
   const sponsorClient = Sponsor.createClient()
   const sponsorAccount = await sponsorClient.getSponsorAccount()
 
-  if (!sponsorAccount) {
-    if (targetWorkspace !== 'master') {
-      throw createFlowIssueError('Can only set initial edition in master workspace')
-    }
-    await promptSwitchToAccount('vtex', true)
-  } else {
-    if (targetWorkspace === 'master') {
-      await promptWorkspaceMaster(targetWorkspace)
-    }
-    await promptSwitchToAccount(sponsorAccount, false)
+  const switched = await confirmAndSwitchEnvironment(sponsorAccount, targetAccount, targetWorkspace)
+  if (!switched) {
+    return
   }
 
   try {
-    const sponsorClientForSponsorAccount = Sponsor.createClient()
-    await sponsorClientForSponsorAccount.setEdition(targetAccount, targetWorkspace, edition)
-
+    if (!(await trySetEdition(sponsorAccount, targetAccount, targetWorkspace, edition))) {
+      return
+    }
     log.info(`Successfully changed edition${workspaceNotice} of account ${chalk.blue(targetAccount)}.`)
-  } catch (ex) {
+  } catch (err) {
     log.error(`Failed to change edition of account ${chalk.blue(targetAccount)}.`)
-    throw ex
+    throw err
   } finally {
     if (autoSwitchBack) {
       await returnToPreviousAccount({ previousAccount, previousWorkspace, promptConfirmation: false })
