cf7e79f483c7bb36e7231745877d8d3fe9543a28
William Azevedo da Silva <william.silva@vtex.com.br>
2021-05-10 11:00:10 -0400

    Use remote configs

--- src/modules/init/index.ts
+++ src/modules/init/index.ts
@@ -1,6 +1,7 @@
 import chalk from 'chalk'
 import enquirer from 'enquirer'
 import { keys, prop, reject, test } from 'ramda'
+import { FeatureFlag } from '../../api/modules'
 
 import log from '../../api/logger'
 import { promptConfirm } from '../../api/modules/prompts'
@@ -9,80 +10,17 @@ import * as git from './git'
 import { SessionManager } from '../../api/session/SessionManager'
 import { Messages } from '../../lib/constants/Messages'
 
-const VTEX_APPS = 'vtex-apps'
+// const VTEX_APPS = 'vtex-apps'
 
-const VTEXInternalTemplates = [
-  // Only show these templates for VTEX e-mail users.
-  'graphql-example',
-  'payment-provider-example',
-  'admin-example',
-  'delivery-theme',
-  'service-example',
-  'render-guide',
-  'masterdata-graphql-guide',
-  'support app',
-  'react-guide',
-]
+// Only show these templates for VTEX e-mail users.
+const VTEXInternalTemplates = FeatureFlag.getSingleton().getFeatureFlagInfo<string[]>('VTEXInternalTemplates')
 
 interface Template {
   repository: string
   organization: string
 }
 
-const templates: Record<string, Template> = {
-  'graphql-example': {
-    repository: 'graphql-example',
-    organization: VTEX_APPS,
-  },
-  'payment-provider-example': {
-    repository: 'payment-provider-example',
-    organization: VTEX_APPS,
-  },
-  'admin-example': {
-    repository: 'admin-example',
-    organization: VTEX_APPS,
-  },
-  store: {
-    repository: 'minimum-boilerplate-theme',
-    organization: VTEX_APPS,
-  },
-  'delivery-theme': {
-    repository: 'delivery-theme',
-    organization: VTEX_APPS,
-  },
-  'service-example': {
-    repository: 'service-example',
-    organization: VTEX_APPS,
-  },
-  'render-guide': {
-    repository: 'render-guide',
-    organization: VTEX_APPS,
-  },
-  'masterdata-graphql-guide': {
-    repository: 'masterdata-graphql-guide',
-    organization: VTEX_APPS,
-  },
-  'edition app': {
-    repository: 'edition-hello',
-    organization: VTEX_APPS,
-  },
-  'support app': {
-    repository: 'hello-support',
-    organization: VTEX_APPS,
-  },
-  'react-guide': {
-    repository: 'react-app-template',
-    organization: VTEX_APPS,
-  },
-  'checkout-ui-settings': {
-    repository: 'checkout-ui-settings',
-    organization: VTEX_APPS,
-  },
-  'service-worker-example': {
-    repository: 'service-worker-example',
-    organization: VTEX_APPS,
-  },
-}
+const templates = FeatureFlag.getSingleton().getFeatureFlagInfo<Record<string, Template>>('templates')
 
 const getTemplates = () =>
   // Return all templates if user's e-mail is `...@vtex...`.
