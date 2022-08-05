590eeda3a6da44b00288c3e59ab328d77b9436dd
Charles Santos <thyarles@gmail.com>
2022-08-05 10:44:57 -0300

    Avoid feedback form if in CI

--- src/nps.ts
+++ src/nps.ts
@@ -15,6 +15,10 @@ const choices = {
 
 export async function checkAndOpenNPSLink() {
   const nextFeedbackDateString = getNextFeedbackDate()
+  const ci = process.env.CI ?? false
+
+  // If CI env, skip the feedback always
+  if (ci) return
 
   if (!nextFeedbackDateString) {
     // If the user is starting to use the tool, wait 1 week to ask for feedback.
